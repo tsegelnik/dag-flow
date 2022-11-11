1652724b2ec66aba844e8219cd46c7cf84c68dd0
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-11-11 15:52:54 +0300

    tests: rename `test.py` as it is not seen by pytest by default. Comment test7/test8.

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
+from dagflow.node import Node
 from dagflow.input import Input
+from dagflow.node import Node
 from dagflow.output import Output
 from dagflow.nodes import FunctionNode
 from dagflow.graph import Graph
 from dagflow.wrappers import *
 
 def test_01():
-    i = Input('input', None)
-    o = Output('output', None)
+    i = Input('input', Node('n1'))
+    o = Output('output', Node('n2'))
 
     o >> i
 
@@ -24,6 +26,8 @@ def test_02():
     n2._add_output('o1')
 
     n1 >> n2
+
+
 
 def test_03():
     n1 = FunctionNode('node1')
@@ -70,47 +74,49 @@ def test_06():
 
     (out1, out2) >> n2
 
-def test_07():
-    g = Graph()
-    n1 = g.add_node('node1')
-    n2 = g.add_node('node2')
-    g._wrap_fcns(toucher, printer)
-
-    out1 = n1._add_output('o1')
-    out2 = n1._add_output('o2')
-
-    _, final = n2._add_pair('i1', 'o1')
-    n2._add_input('i2')
-
-    (out1, out2) >> n2
-
-    final.data
-
-def test_08():
-    g = Graph()
-    n1 = g.add_node('node1')
-    n2 = g.add_node('node2')
-    n3 = g.add_node('node3')
-    g._wrap_fcns(toucher, printer)
-
-    out1 = n1._add_output('o1')
-    out2 = n1._add_output('o2')
-
-    _, out3 = n2._add_pair('i1', 'o1')
-    n2._add_input('i2')
-
-    _, final = n3._add_pair('i1', 'o1')
-
-    (out1, out2) >> n2
-    out3 >> n3
-
-    print()
-    final.data
-
-    print('Taint n2')
-    n2.taint()
-    final.data
-
-    print('Taint n3')
-    n3.taint()
-    final.data
+# def test_07():
+#     g = Graph()
+#     n1 = g.add_node('node1')
+#     n2 = g.add_node('node2')
+#     g._wrap_fcns(toucher, printer)
+#
+#     out1 = n1._add_output('o1')
+#     out2 = n1._add_output('o2')
+#
+#     _, final = n2._add_pair('i1', 'o1')
+#     n2._add_input('i2')
+#
+#     (out1, out2) >> n2
+#     g.close()
+#
+#     final.data
+#
+# def test_08():
+#     g = Graph()
+#     n1 = g.add_node('node1')
+#     n2 = g.add_node('node2')
+#     n3 = g.add_node('node3')
+#     g._wrap_fcns(toucher, printer)
+#
+#     out1 = n1._add_output('o1')
+#     out2 = n1._add_output('o2')
+#
+#     _, out3 = n2._add_pair('i1', 'o1')
+#     n2._add_input('i2')
+#
+#     _, final = n3._add_pair('i1', 'o1')
+#
+#     (out1, out2) >> n2
+#     out3 >> n3
+#     g.close()
+#
+#     print()
+#     final.data
+#
+#     print('Taint n2')
+#     n2.taint()
+#     final.data
+#
+#     print('Taint n3')
+#     n3.taint()
+#     final.data
