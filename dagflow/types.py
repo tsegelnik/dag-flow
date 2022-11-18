51b5b00b2c3e8b17775073985a93d77b985b6b5f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-11-18 10:47:40 +0300

    chore: remove ReferenceType

--- dagflow/types.py
+++ dagflow/types.py
@@ -1,9 +1,9 @@
 from typing import TypeVar
-from weakref import ReferenceType
+# from weakref import ReferenceType
 
 NodeT = TypeVar('NodeT', bound='Node')
 InputT = TypeVar('InputT', bound='Input')
 OutputT = TypeVar('OutputT', bound='Output')
 
-NodeRefT = ReferenceType[NodeT]
+# NodeRefT = ReferenceType[NodeT]
 
