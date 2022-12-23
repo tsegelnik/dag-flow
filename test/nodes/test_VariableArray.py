6f1d1243c9dea6e66137123bdd4032b46bb072e6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-12-23 10:37:14 +0300

    test: `VariableArray` unit test

---
+++ test/nodes/test_VariableArray.py
@@ -0,0 +1,44 @@
+from dagflow.lib.VariableArray import VariableArray
+from dagflow.lib.Sum import Sum
+
+def test_VariableArray_01():
+	value = 123.
+	array_in = (value, )
+	array_alt = (value+1, )
+	va = VariableArray('test', array_in)
+	sm = Sum('sum')
+	va >> sm
+	va.close()
+	sm.close()
+
+	output = va.outputs[0]
+	output2 = sm.outputs[0]
+
+	assert va.tainted==True
+	assert sm.tainted==True
+	assert output.data[0]==value
+	assert output2.data[0]==value
+	assert va.tainted==False
+	assert sm.tainted==False
+
+	assert va.set(array_in, check_taint=True)==False
+	assert va.tainted==False
+	assert sm.tainted==False
+	assert (output.data==array_in).all()
+	assert (output2.data==array_in).all()
+
+	assert va.set(array_in)==True
+	assert va.tainted==False
+	assert sm.tainted==True
+	assert (output.data==array_in).all()
+	assert (output2.data==array_in).all()
+	assert va.tainted==False
+	assert sm.tainted==False
+
+	assert va.set(array_alt, check_taint=True)==True
+	assert va.tainted==False
+	assert sm.tainted==True
+	assert (output.data==array_alt).all()
+	assert (output2.data==array_alt).all()
+	assert va.tainted==False
+	assert sm.tainted==False
