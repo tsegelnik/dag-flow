cd3ee46bac186b9c02ac2b50b9f42d97dd07caee
Tsegelnik Nikita <tsegelnik@jinr.ru>
2022-12-27 16:05:38 +0300

    avoid default mutable arg and update codestyle

--- test/test_graph.py
+++ test/test_graph.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-
-
 from dagflow.graph import Graph
 from dagflow.graphviz import GraphDot
 from dagflow.printl import current_level, set_prefix_function
@@ -10,6 +8,7 @@ set_prefix_function(
     lambda: "{:<2d} ".format(current_level()),
 )
 nodeargs = dict(typefunc=lambda: True)
+
 
 def test_01():
     """Simple test of the graph plotter"""
@@ -22,9 +21,9 @@ def test_01():
     out1 = n1._add_output("o1", allocatable=False)
     out2 = n1._add_output("o2", allocatable=False)
 
-    _, out3 = n2._add_pair("i1", "o1", output_kws={'allocatable': False})
+    _, out3 = n2._add_pair("i1", "o1", output_kws={"allocatable": False})
     n2._add_input("i2")
-    n3._add_pair("i1", "o1", output_kws={'allocatable': False})
+    n3._add_pair("i1", "o1", output_kws={"allocatable": False})
 
     print(f"{out1=}, {out2=}")
     (out1, out2) >> n2
@@ -46,10 +45,10 @@ def test_02():
     out1 = n1._add_output("o1", allocatable=False)
     out2 = n1._add_output("o2", allocatable=False)
 
-    _, out3 = n2._add_pair("i1", "o1", output_kws={'allocatable': False})
+    _, out3 = n2._add_pair("i1", "o1", output_kws={"allocatable": False})
     n2._add_input("i2")
 
-    _, final = n3._add_pair("i1", "o1", output_kws={'allocatable': False})
+    _, final = n3._add_pair("i1", "o1", output_kws={"allocatable": False})
 
     (out1, out2) >> n2
     out3 >> n3
@@ -74,9 +73,9 @@ def test_02a():
 
     out1 = n1._add_output("o1", allocatable=False)
 
-    in2, out2 = n2._add_pair("i1", "o1", output_kws={'allocatable': False})
-    in3, out3 = n3._add_pair("i1", "o1", output_kws={'allocatable': False})
-    in4, out4 = n4._add_pair("i1", "o1", output_kws={'allocatable': False})
+    in2, out2 = n2._add_pair("i1", "o1", output_kws={"allocatable": False})
+    in3, out3 = n3._add_pair("i1", "o1", output_kws={"allocatable": False})
+    in4, out4 = n4._add_pair("i1", "o1", output_kws={"allocatable": False})
 
     out1.repeat() >> (in2, in3, in4)
     g.close()
