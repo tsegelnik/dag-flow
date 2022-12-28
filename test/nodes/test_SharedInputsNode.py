b8508f5f9279f79b6d7bf6e75bc70d71d81fed42
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-12-28 17:28:16 +0300

    test: extend unit tests for View/SharedInputsNode

--- test/nodes/test_SharedInputsNode.py
+++ test/nodes/test_SharedInputsNode.py
@@ -3,81 +3,87 @@ from numpy import arange
 
 from dagflow.graph import Graph
 from dagflow.lib.SharedInputsNode import SharedInputsNode
+from dagflow.lib.View import View
 from dagflow.lib.Array import Array
-from dagflow.lib.Sum import Sum
+from dagflow.exception import AllocationError
 from dagflow.graphviz import savegraph
+from pytest import raises
 
-def test_SharedInputsNode_00(debug_graph):
+def test_SharedInputsNode_00(debug_graph=False):
     array = arange(12.0).reshape(3, 4)
     with Graph(debug=debug_graph, close=True) as graph:
-        initial = Array("array 1", array)
-        initial2 = Array("array 2", array)
-        sum = Sum("copy 1")
-        sum2 = Sum("copy 2")
-        view = SharedInputsNode("view")
+        initial = Array("array 1", array, mode='fill')
+        initial2 = Array("array 2", array, mode='fill')
+        sharedinput = SharedInputsNode("shared input")
+        view = View("view")
 
-        initial >> sum >> view
-        initial2 >> sum2 >> view
-
-    graph.print()
+        initial >> sharedinput
+        initial2 >> sharedinput >> view
 
     output_array = initial.outputs[0]
-    input_sum = sum.inputs[0]
-    output_sum = sum.outputs[0]
-    input_sum2 = sum2.inputs[0]
-    output_sum2 = sum2.outputs[0]
-    input_view = view.inputs[0]
+    output_array2 = initial2.outputs[0]
+    input_sharedinput = sharedinput.inputs[0]
+    output_sharedinput = sharedinput.outputs[0]
     output_view = view.outputs[0]
 
-    assert output_array.allocatable == False
-    assert output_sum.allocatable == True
-    assert output_sum2.allocatable == True
+    assert output_array.allocatable == True
+    assert output_array2.allocatable == True
+    assert output_sharedinput.allocatable == False
+    assert input_sharedinput.allocatable == True
     assert output_view.allocatable == False
-    assert input_sum.allocatable == False
-    assert input_sum2.allocatable == False
-    assert input_view.allocatable == True
 
-    assert input_view._own_data is view._data
-    assert output_view._data is view._data
-    assert output_sum._data is view._data
-    assert output_sum2._data is view._data
+    assert input_sharedinput._own_data is sharedinput._data
+    assert output_sharedinput._data is sharedinput._data
+    assert output_view._data is sharedinput._data
 
     assert initial.tainted == True
     assert initial2.tainted == True
-    assert sum.tainted == True
-    assert sum2.tainted == True
+    assert sharedinput.tainted == True
     assert view.tainted == True
 
+    assert (output_sharedinput.data == array).all()
     assert (output_view.data == array).all()
 
     assert initial.tainted == False
     assert initial2.tainted == False
-    assert sum.tainted == False
-    assert sum2.tainted == False
+    assert sharedinput.tainted == False
     assert view.tainted == False
 
     initial._data[:] = 1
     initial.taint()
     assert initial.tainted == True
     assert initial2.tainted == False
-    assert sum.tainted == True
-    assert sum2.tainted == False
+    assert sharedinput.tainted == True
     assert view.tainted == True
+    assert (output_sharedinput.data == 1).all()
     assert (output_view.data == 1).all()
-    assert (output_sum.data == 1).all()
-    assert (output_sum2.data == 1).all()
 
     initial2._data[:] = 2
     initial2.taint()
     assert initial.tainted == False
     assert initial2.tainted == True
-    assert sum.tainted == False
-    assert sum2.tainted == True
+    assert sharedinput.tainted == True
     assert view.tainted == True
+    assert (output_sharedinput.data == 2).all()
     assert (output_view.data == 2).all()
-    assert (output_sum.data == 2).all()
-    assert (output_sum2.data == 2).all()
 
     view.touch()
-
     savegraph(graph, "output/test_SharedInputsNode_00.png")
+
+def test_SharedInputsNode_01():
+    array = arange(12.0).reshape(3, 4)
+    with Graph() as graph:
+        initial = Array("array 1", array, mode='fill')
+        initial2 = Array("array 2", array, mode='fill')
+        sharedinput = SharedInputsNode("shared input")
+        sharedinput2 = SharedInputsNode("shared input 2")
+
+        initial >> sharedinput
+        initial2 >> sharedinput
+
+        sharedinput >> sharedinput2
+
+    savegraph(graph, "output/test_SharedInputsNode_01.png")
+
+    with raises(AllocationError):
+        graph.close()
--- test/nodes/test_View.py
+++ test/nodes/test_View.py
@@ -17,29 +17,38 @@ def test_View_00():
     Use one-line code for connecting the nodes
     """
     array = arange(5.0)
-    with Graph(debug=debug) as graph:
+    with Graph(close=True) as graph:
         initial = Array('array', array)
         view = View("view")
+        view2 = View("view2")
 
-    initial >> view
-    graph.close()
+        initial >> view >> view2
 
     assert initial.tainted==True
     assert view.tainted==True
+    assert view2.tainted==True
 
-    result = view.outputs['view'].data
+    result = view.get_data()
+    result2 = view2.get_data()
     assert (result==array).all()
+    assert (result2==array).all()
     assert view.tainted==False
+    assert view2.tainted==False
     assert initial.tainted==False
 
     d1=initial.outputs[0]._data
     d2=view.outputs[0]._data
+    d3=view2.outputs[0]._data
     assert (d1==d2).all()
+    assert (d1==d3).all()
     d1[:]=-1
     assert (d2==-1).all()
+    assert (d3==-1).all()
 
     initial.taint()
     assert initial.tainted==True
     assert view.tainted==True
+    assert view2.tainted==True
 
+    view2.touch()
     savegraph(graph, "output/test_View_00.png")
