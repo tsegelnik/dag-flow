d5e53cc9b350eaeb0a9b8c669b2c7ced44792796
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-02-03 10:48:08 +0300

    test: Cholesky

--- test/nodes/test_Cholesky.py
+++ test/nodes/test_Cholesky.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+from dagflow.exception import TypeFunctionError
 from dagflow.graph import Graph
 from dagflow.lib.Array import Array
 from dagflow.lib.Cholesky import Cholesky
 import numpy as np
+from pytest import raises
 
 def test_Cholesky_00():
     inV = np.array([
@@ -27,3 +29,24 @@ def test_Cholesky_00():
 
     assert np.allclose(inL, result, atol=0, rtol=0)
 
+def test_Cholesky_01_typefunctions():
+    inV = np.array([
+        [10, 2,   1],
+        [ 2, 12,  3],
+        ], dtype='d')
+
+    with Graph() as g1:
+        V1 = Array('V1', inV, mode='store')
+        chol1 = Cholesky('Cholesky')
+        V1 >> chol1
+
+    with Graph() as g2:
+        V2 = Array('V2', inV[0], mode='store')
+        chol2 = Cholesky('Cholesky')
+        V2 >> chol1
+
+    with raises(TypeFunctionError):
+        g1.close()
+
+    with raises(TypeFunctionError):
+        g2.close()
