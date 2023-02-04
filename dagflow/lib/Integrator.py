8c68d00b9809673cc242c654a3776123eca19e03
Tsegelnik Nikita <tsegelnik@jinr.ru>
2023-02-04 21:23:57 +0300

    adding 2d integration, update test

--- dagflow/lib/Integrator.py
+++ dagflow/lib/Integrator.py
@@ -4,21 +4,46 @@ from numpy import multiply, zeros
 from ..exception import InitializationError
 from ..input_extra import MissingInputAddEach
 from ..nodes import FunctionNode
-from ..typefunctions import check_has_inputs
+from ..typefunctions import check_has_inputs, check_input_dimension
 
 
 @jit(nopython=True)
-def _integrate(data, weighted, orders):
+def _integrate1d(data, weighted, orders):
     iprev = 0
     for i, order in enumerate(orders):
         inext = iprev + order
-        data[i] = sum(weighted[iprev:inext])
+        data[i] = weighted[iprev:inext].sum()
+        iprev = inext
+
+
+# TODO: Numby doesn't like this.
+# I think there is a problem with `orders`
+# because of `dtype=object` due to non-equal shape
+#@jit(nopython=True)
+def _integrate2d(data, weighted, orders):
+    iprev = 0
+    for i, orderx in enumerate(orders[0]):
+        inext = iprev + orderx
+        jprev = 0
+        for j, ordery in enumerate(orders[1]):
+            jnext = jprev + ordery
+            data[i, j] = weighted[iprev:inext, jprev:jnext].sum()
+            jprev = jnext
         iprev = inext
 
 
 class Integrator(FunctionNode):
     """
-    Integrator info
+    The `Integrator` node performs integration (summation)
+    of every input within the `weight` and `orders` inputs.
+    The `precision` of integration can be chosen by `precision` kwarg.
+
+    The `Integrator` has two modes: `1d` and `2d`.
+    The mode is chosen by `mode` kwarg.
+
+    For `2d` integration arrays are like `array([...], [...])`.
+
+    Note that the `Integrator` preallocates temporary buffer.
     """
 
     def __init__(self, *args, **kwargs):
@@ -35,9 +60,7 @@ class Integrator(FunctionNode):
         super().__init__(*args, **kwargs)
         self._add_input("weights", positional=False)
         self._add_input("orders", positional=False)
-        self._functions.update(
-            {"1d": self._fcn_1d, "2d": self._fcn_2d}
-        )
+        self._functions.update({"1d": self._fcn_1d, "2d": self._fcn_2d})
 
     @property
     def mode(self):
@@ -50,20 +73,43 @@ class Integrator(FunctionNode):
     def _typefunc(self) -> None:
         """The function to determine the dtype and shape"""
         check_has_inputs(self)
+        check_input_dimension(
+            self, slice(None), 1 if self._mode == "1d" else 2
+        )
         self.fcn = self._functions[self._mode]
-        for output in self.outputs:
+        for output, input in zip(self.outputs, self.inputs):
             output._dtype = self._precision
-            output._shape = self.inputs["orders"].shape
+            output._shape = input.shape
+
+    def post_allocate(self):
+        """Find the longest input and allocate the `buffer`"""
+        shape = self.inputs[0].shape
+        dtype = self.inputs[0].dtype
+        if len(self.inputs) > 1:
+            for input in self.inputs[1:]:
+                for i, j in zip(input.shape, shape):
+                    if i > j:
+                        shape = input.shape
+                        dtype = input.dtype
+                        break
+                    elif i < j:
+                        break
+        self.__buffer = zeros(shape, dtype)
 
     def _fcn_1d(self, _, inputs, outputs):
         weights = inputs["weights"].data
         orders = inputs["orders"].data
-        weighted = zeros(inputs[0].shape, inputs[0].dtype)
-        for input, output in zip(inputs, outputs):
-            multiply(input.data, weights, out=weighted)
-            _integrate(output.data, weighted, orders)
+        for input, output in zip(inputs.iter_data(), outputs.iter_data()):
+            multiply(input, weights, out=self.__buffer)
+            _integrate1d(output, self.__buffer, orders)
         if self.debug:
-            return [output.data for output in outputs]
+            return [outputs.iter_data()]
 
     def _fcn_2d(self, _, inputs, outputs):
-        pass
+        weights = inputs["weights"].data
+        orders = inputs["orders"].data
+        for input, output in zip(inputs.iter_data(), outputs.iter_data()):
+            multiply(input, weights, out=self.__buffer)
+            _integrate2d(output, self.__buffer, orders)
+        if self.debug:
+            return [outputs.iter_data()]
---
+++ test/nodes/test_Integrator.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from dagflow.graph import Graph
+from dagflow.lib.Array import Array
+from dagflow.lib.Integrator import Integrator
+
+
+def test_Integrator_00(debug_graph):
+    with Graph(debug=debug_graph, close=True):
+        arr1 = Array("array", [1, 2, 3])
+        arr2 = Array("array", [3, 2, 1])
+        weights = Array("weights", [1, 1, 1])
+        orders = Array("orders", [1, 0, 1])
+        integrator = Integrator("integrator")
+        arr1 >> integrator
+        arr2 >> integrator
+        weights >> integrator("weights")
+        orders >> integrator("orders")
+    assert all(integrator.outputs[0].data == [1, 0, 2])
+    assert all(integrator.outputs[1].data == [3, 0, 2])
+
+
+def test_Integrator_01(debug_graph):
+    unity = [1, 1, 1]
+    with Graph(debug=debug_graph, close=True):
+        arr1 = Array("array", [unity, unity])
+        arr2 = Array("array", [[1, 2, 3], [1, 2, 3]])
+        weights = Array("weights", [unity, unity])
+        orders = Array("orders", [[1, 1], unity])
+        integrator = Integrator("integrator", mode="2d")
+        arr1 >> integrator
+        arr2 >> integrator
+        weights >> integrator("weights")
+        orders >> integrator("orders")
+    assert (integrator.outputs[0].data == [[1, 1, 1], [1, 1, 1]]).all()
+    assert (integrator.outputs[1].data == [[1, 2, 3], [1, 2, 3]]).all()
+
+
+def test_Integrator_02(debug_graph):
+    unity = [1, 1, 1]
+    with Graph(debug=debug_graph, close=True):
+        arr1 = Array("array", [unity, unity])
+        arr2 = Array("array", [[1, 2, 3], [1, 2, 3]])
+        weights = Array("weights", [[0, 1, 2], [3, 4, 5]])
+        orders = Array("orders", [[0, 2], [2, 1, 0]])
+        integrator = Integrator("integrator", mode="2d")
+        arr1 >> integrator
+        arr2 >> integrator
+        weights >> integrator("weights")
+        orders >> integrator("orders")
+    assert (integrator.outputs[0].data == [[0, 0, 0], [8, 7, 0]]).all()
+    assert (integrator.outputs[1].data == [[0, 0, 0], [13, 21, 0]]).all()
