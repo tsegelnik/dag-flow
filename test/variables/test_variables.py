5d58386d4d5a28cd9d280daec72b17d1cb42afdd
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-02-10 17:27:39 +0300

    test: GaussianParameters

--- test/variables/test_variables.py
+++ test/variables/test_variables.py
@@ -5,49 +5,65 @@ from dagflow.lib.NormalizeCorrelatedVars import NormalizeCorrelatedVars
 from dagflow.variable import Parameters, GaussianParameters
 from dagflow.graph import Graph
 from dagflow.graphviz import savegraph
+from dagflow.exception import CriticalError
 
-from numpy import square
+from numpy import square, allclose
 import pytest
 
 # def test_variables_00_parameter() -> None:
 #     pass
 
-
 @pytest.mark.parametrize('mode', ('single', 'uncorr', 'cov', 'cov1d'))
+# @pytest.mark.parametrize('mode', ('cov',))
 def test_variables_00_variable(mode) -> None:
-    value_in = [1.1, 1.8, 5.0]
-    central_in = [1.0, 2.0, 3.0]
-    sigma_in = [1.0, 0.5, 2.0]
-    var_in = square(sigma_in)
+    value_in    = [1.1, 1.8, 5.0]
+    central_in  = [1.0, 2.0, 3.0]
+    sigma_in    = [1.0, 0.5, 2.0]
+    corrs_in    = [-0.1, 0.5, -0.9] # 01, 02, 12
+    variance_in = square(sigma_in)
 
-    with Graph(close=True) as graph:
-        if mode=='single':
-            value   = Array("variable", value_in[:1],   mode='store_weak', mark='v')
-            central = Array("central",  central_in[:1], mode='store_weak', mark='v₀')
-        else:
-            value   = Array("variable", value_in,       mode='store_weak', mark='v')
-            central = Array("central",  central_in,     mode='store_weak', mark='v₀')
+    if mode=='single':
+        value_in = value_in[:1]
+        central_in = central_in[:1]
+        sigma_in = sigma_in[:1]
 
-        if mode=='single':
-            sigma   = Array("central",  [1.0], mode='store_weak', mark='σ')
-            gp = GaussianParameters(value, central, sigma=sigma)
-        elif mode=='uncorr':
-            sigma   = Array("sigma",    sigma_in, mode='store_weak', mark='σ')
+    with Graph(close=False) as graph:
+        value   = Array("variable", value_in,       mode='store_weak', mark='v')
+        central = Array("central",  central_in,     mode='store_weak', mark='v₀')
+
+        if mode in ('single', 'uncorr'):
+            sigma   = Array("central",  sigma_in, mode='store_weak', mark='σ')
             gp = GaussianParameters(value, central, sigma=sigma)
         elif mode=='cov':
             covariance = Array("covariance", [
-                                   [var_in[0], 0.0,       0.5],
-                                   [0.0,       var_in[1], 1.0],
-                                   [0.5,       1.0,       var_in[2]],
+                    [variance_in[0],                      corrs_in[0]*sigma_in[0]*sigma_in[1], corrs_in[1]*sigma_in[0]*sigma_in[2]],
+                    [corrs_in[0]*sigma_in[0]*sigma_in[1], variance_in[1],                      corrs_in[2]*sigma_in[1]*sigma_in[2]],
+                    [corrs_in[1]*sigma_in[0]*sigma_in[2], corrs_in[2]*sigma_in[1]*sigma_in[2], variance_in[2]]
                                 ],
                                mark='V',
                                mode='store_weak')
             gp = GaussianParameters(value, central, covariance=covariance)
         elif mode=='cov1d':
-            covariance = Array("covariance", var_in, mark='diag(V)', mode='store_weak')
+            covariance = Array("covariance", variance_in, mark='diag(V)', mode='store_weak')
             gp = GaussianParameters(value, central, covariance=covariance)
+        # elif mode=='cor':
+        #     correlation = Array("correlation", [
+        #         [1.0,         corrs_in[0], corrs_in[1]],
+        #         [corrs_in[0], 1.0,         corrs_in[2]],
+        #         [corrs_in[1], corrs_in[2], 1.0],
+        #         ], mark='C', mode='store_weak')
+        #     gp = GaussianParameters(value, central, correlation=correlation)
+        else:
+            raise RuntimeError(f"Invalid mode {mode}")
+
+    try:
+        graph.close()
+    except CriticalError as error:
+        savegraph(graph, f"output/test_variables_00_{mode}.png")
+        raise error
+
+    value_out = gp._backward_node.get_data(0)
+    assert allclose(value_in, value_out, atol=0, rtol=0)
 
     savegraph(graph, f"output/test_variables_00_{mode}.png")
-
-    # import IPython; IPython.embed(colors='neutral')
 
