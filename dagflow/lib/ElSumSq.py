97087e9f03ac1fd45376f0501dcbc3a00252ac91
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-02-27 16:42:00 +0300

    feat: ElSumSq - elementwise sum of squares

---
+++ dagflow/lib/ElSumSq.py
@@ -0,0 +1,44 @@
+from numpy import ndarray
+from numpy.typing import NDArray
+
+from numba import jit
+from ..input_extra import MissingInputAddOne
+from ..nodes import FunctionNode
+from ..typefunctions import (
+    check_has_inputs,
+    eval_output_dtype,
+    copy_input_shape_to_output,
+    check_inputs_same_dtype,
+    AllPositionals
+)
+
+@jit(nopython=True)
+def _sumsq(data: NDArray, out: NDArray):
+    sm = 0.0
+    for v in data:
+        sm+=v*v
+    out[0]+=sm
+
+class ElSumSq(FunctionNode):
+    """Sum of the squared of all the inputs"""
+
+    _buffer: ndarray
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault(
+            "missing_input_handler", MissingInputAddOne(output_fmt="result")
+        )
+        super().__init__(*args, **kwargs)
+
+    def _fcn(self, _, inputs, outputs):
+        out = outputs["result"].data
+        out[0] = 0.0
+        for input in inputs:
+            _sumsq(input.data, out)
+        return out
+
+    def _typefunc(self) -> None:
+        """A output takes this function to determine the dtype and shape"""
+        check_has_inputs(self)
+        check_inputs_same_dtype(self)
+        eval_output_dtype(self, AllPositionals, "result")
+        self.outputs[0]._shape=(1,)
