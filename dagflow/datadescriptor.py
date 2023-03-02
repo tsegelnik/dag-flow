0a2c2d837027b919962e9609a02ddcb4159d9bfb
Tsegelnik Nikita <tsegelnik@jinr.ru>
2023-03-02 13:17:45 +0300

    [issue: #31] design DataDescriptor and Axis classes, adding new types

---
+++ dagflow/datadescriptor.py
@@ -0,0 +1,46 @@
+from numpy.typing import DTypeLike
+
+from .types import ShapeLikeT, EdgesLikeT
+
+
+class Axis:
+    """The axis class stores `edges` and `nodes`"""
+
+    def __init__(
+        self,
+        edges: EdgesLikeT = None,
+        nodes: EdgesLikeT = None,
+    ) -> None:
+        self._edges = edges
+        self._nodes = nodes
+
+    @property
+    def edges(self) -> EdgesLikeT:
+        return self._edges
+
+    @property
+    def nodes(self) -> EdgesLikeT:
+        return self._nodes
+
+
+class DataDescriptor:
+    """The data descriptor class stores `dtype`, `shape` and `axis` information"""
+
+    def __init__(
+        self,
+        dtype: DTypeLike = None,
+        shape: ShapeLikeT = None,
+        axis_edges: EdgesLikeT = None,
+        axis_nodes: EdgesLikeT = None,
+    ) -> None:
+        self._shape = shape
+        self._dtype = dtype
+        self._axis = Axis(axis_edges, axis_nodes)
+
+    @property
+    def dtype(self) -> DTypeLike:
+        return self._dtype
+
+    @property
+    def shape(self) -> ShapeLikeT:
+        return self._shape
--- dagflow/types.py
+++ dagflow/types.py
@@ -1,7 +1,10 @@
-from typing import TypeVar
+from typing import Tuple, TypeVar, Union
 
 NodeT = TypeVar("NodeT", bound="Node")
 InputT = TypeVar("InputT", bound="Input")
 InputsT = TypeVar("InputsT", bound="Inputs")
 OutputT = TypeVar("OutputT", bound="Output")
 OutputsT = TypeVar("OutputsT", bound="Outputs")
+
+ShapeLikeT = Union[Tuple[int, ...], None] # like DTypeLike from Numpy
+EdgesLikeT = Union[Tuple[OutputT], None]
