ac2e758e310f4f8973df91c9daae0f087b295934
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-03 14:24:16 +0300

    draft: variables, finalize labels validation

--- test/variables/test_load_variables.py
+++ test/variables/test_load_variables.py
@@ -5,27 +5,52 @@ from typing import Any, Union
 from schema import Schema, Or, Optional, Use, And, Schema, SchemaError
 
 class NestedSchema(object):
-    __slots__ = ('_schema',)
+    __slots__ = ('_schema', '_processdicts')
     _schema: Union[Schema,object]
+    _processdicts: bool
 
-    def __init__(self, schema: Union[Schema,object]):
+    def __init__(self, /, schema: Union[Schema,object], *, processdicts: bool=False):
         self._schema = schema
+        self._processdicts = processdicts
 
     def validate(self, data: Any) -> Any:
         if not isinstance(data, dict):
             return self._schema.validate(data)
 
+        if self._processdicts:
+            dtout = {}
+            for key, subdata in data.items():
+                dtout[key] = self._process_dict((key,), subdata)
+
+            return dtout
+
         dtin = DictWrapper(data)
         dtout = DictWrapper({})
         for key, subdata in dtin.walkitems():
-            try:
-                subdata_new = self._schema.validate(subdata, _is_event_schema=False)
-            except SchemaError as err:
-                key = ".".join(str(k) for k in key)
-                raise SchemaError(f'The key "{key}" has invalid value "{subdata}":\n{err.args[0]}') from err
-            dtout[key] = subdata_new
+            dtout[key] = self._process_element(key, subdata)
 
         return dtout.object
+
+    def _process_element(self, key, subdata: Any) -> Any:
+        try:
+            return self._schema.validate(subdata, _is_event_schema=False)
+        except SchemaError as err:
+            key = ".".join(str(k) for k in key)
+            raise SchemaError(f'Key "{key}" has invalid value "{subdata}":\n{err.args[0]}') from err
+
+    def _process_dict(self, key, data: Any) -> Any:
+        if not isinstance(data, dict):
+            return self._schema.validate(data)
+
+        try:
+            return self._schema.validate(data, _is_event_schema=False)
+        except SchemaError:
+            pass
+
+        return {
+            subkey: self._process_dict(key+(subkey,), subdata) for subkey, subdata in data.items()
+        }
+
 
 class ParsCfgHasProperFormat(object):
     def validate(self, data: dict) -> dict:
@@ -43,7 +68,7 @@ class ParsCfgHasProperFormat(object):
                 if nelements==1: continue
 
             key = ".".join(str(k) for k in key)
-            raise SchemaError(f'The key "{key}" has  value "{subdata}"" inconsistent with format "{format}"')
+            raise SchemaError(f'Key "{key}" has  value "{subdata}"" inconsistent with format "{format}"')
 
         return data
 
@@ -51,16 +76,15 @@ IsNumber = Or(float, int, error='Invalid number "{}", expect int of float')
 IsNumberOrTuple = Or(IsNumber, (IsNumber,), error='Invalid number/tuple {}')
 IsLabel = Or(
     {'text': str, Optional('latex'): str},
-    And(str, Use(lambda s: {'text': s}), error='Invalid string')
+    And(str, Use(lambda s: {'text': s}), error='Invalid string: {}')
 )
 IsValuesDict = NestedSchema(IsNumberOrTuple)
-IsLabelsDict = NestedSchema(IsLabel)
+IsLabelsDict = NestedSchema(IsLabel, processdicts=True)
 def IsFormatOk(format):
-    f1, f2, f3 = None, None, None
-
     if isinstance(format, tuple):
         if len(format)==1:
             f1,=format
+            return f1=='value'
         else:
             if len(format)==2:
                 f1, f3 = format
@@ -74,13 +98,11 @@ def IsFormatOk(format):
 
             if f3 not in ('sigma_absolute', 'sigma_relative', 'sigma_percent'):
                 return False
+
+            return f1 in ('value', 'central')
     else:
-        f1 = format
+        return format=='value'
 
-    if f1 not in ('value', 'central'):
-        return True
-
-    return True
 IsFormat = Schema(IsFormatOk, error='Invalid variable format "{}".')
 IsVarsCfgDict = Schema({
     'variables': IsValuesDict,
@@ -90,64 +112,73 @@ IsVarsCfgDict = Schema({
 
 IsProperVarsCfg = And(IsVarsCfgDict, ParsCfgHasProperFormat())
 
-def process_var_fixed1(vcfg, format, hascentral):
+def process_var_fixed1(vcfg, _, __):
     return {'central': vcfg, 'value': vcfg, 'sigma': None}
 
-def process_var_fixed2(vcfg, format, hascentral):
+def process_var_fixed2(vcfg, format, hascentral) -> dict:
     ret = dict(zip(format, vcfg))
     if hascentral:
-        ret['value'] = ret['central']
+        ret.setdefault('value', ret['central'])
     else:
-        ret['central'] = ret['value']
+        ret.setdefault('central', ret['value'])
     ret['sigma'] = None
     return ret
 
-def process_var_absolute(vcfg, format, hascentral):
+def process_var_absolute(vcfg, format, hascentral) -> dict:
     ret = process_var_fixed2(vcfg, format, hascentral)
     ret['sigma'] = ret['sigma_absolute']
     return ret
 
-def process_var_relative(vcfg, format, hascentral):
+def process_var_relative(vcfg, format, hascentral) -> dict:
     ret = process_var_fixed2(vcfg, format, hascentral)
     ret['sigma'] = ret['sigma_relative']*ret['central']
     return ret
 
-def process_var_percent(vcfg, format, hascentral):
+def process_var_percent(vcfg, format, hascentral) -> dict:
     ret = process_var_fixed2(vcfg, format, hascentral)
     ret['sigma'] = 0.01*ret['sigma_percent']*ret['central']
     return ret
 
-def load_variables(cfg):
-    IsProperVarsCfg.validate(cfg)
+def get_format_processor(format):
+    if isinstance(format, str):
+        return process_var_fixed1
 
-    cfg = DictWrapper(cfg)
+    errfmt = format[-1]
+    if errfmt.startswith('sigma'):
+        if errfmt.endswith('_absolute'):
+            return process_var_absolute
+        elif errfmt.endswith('_relative'):
+            return process_var_relative
+        else:
+            return process_var_percent
+    else:
+        return process_var_fixed2
+
+def iterate_varcfgs(cfg: DictWrapper):
     variablescfg = cfg['variables']
     labelscfg = cfg['labels']
     format = cfg['format']
 
     hascentral = 'central' in format
-    if isinstance(format, str):
-        process = process_var_fixed1
-    else:
-        process = process_var_fixed2
-        errfmt = format[-1]
-        if errfmt.startswith('sigma'):
-            if errfmt.endswith('_absolute'):
-                process = process_var_absolute
-            elif errfmt.endswith('_relative'):
-                process = process_var_relative
-            else:
-                process = process_var_percent
-        else:
-            process = process_var_fixed2
+    process = get_format_processor(format)
+
+    for key, varcfg in variablescfg.walkitems():
+        varcfg = process(varcfg, format, hascentral)
+        varcfg['label'] = labelscfg.get(key, None)
+        yield key, varcfg
+
+def load_variables(acfg):
+    cfg = IsProperVarsCfg.validate(acfg)
+    cfg = DictWrapper(cfg)
 
     ret = DictWrapper({})
-
     print('go')
-    for key, varcfg in variablescfg.walkitems():
+    for key, varcfg in iterate_varcfgs(cfg):
         skey = '.'.join(key)
-        varcfg = process(varcfg, format, hascentral)
         print(skey, varcfg)
+
+
+    return ret
 
 
 cfg1 = {
@@ -171,7 +202,7 @@ cfg1 = {
 cfg2 = {
         'variables': {
             'var1': (1.0, 1.0, 0.1),
-            'var2': (1.0, 1.0, 0.1),
+            'var2': (1.0, 2.0, 0.1),
             'sub1': {
                 'var3': (2.0, 1.0, 0.1)
                 }
@@ -206,10 +237,10 @@ cfg3 = {
 
 cfg4 = {
         'variables': {
-            'var1': (1.0, 1.0, 0.1),
-            'var2': (1.0, 2.0, 0.1),
+            'var1': (1.0, 1.0, 10),
+            'var2': (1.0, 2.0, 10),
             'sub1': {
-                'var3': (2.0, 3.0, 0.1)
+                'var3': (2.0, 3.0, 10)
                 }
             },
         'labels': {
@@ -224,10 +255,10 @@ cfg4 = {
 
 cfg5 = {
         'variables': {
-            'var1': (1.0, 0.1),
-            'var2': (2.0, 0.1),
+            'var1': (1.0, 10),
+            'var2': (2.0, 10),
             'sub1': {
-                'var3': (3.0, 0.1)
+                'var3': (3.0, 10)
                 }
             },
         'labels': {
