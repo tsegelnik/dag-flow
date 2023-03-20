a214b1074368fef14d0eff85bbb47f916fd69c7e
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-20 16:32:20 +0300

    feat: daybay_v0, load variables

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -1,4 +1,7 @@
 from dagflow.bundles.load_variables import load_variables
+from pathlib import Path
 
 def model_dayabay_v0():
-	pass
+	datasource = Path('data/dayabay-v0')
+	vars = load_variables({'load': str(datasource/'parameters/pdg2012.yaml')})
+	print(vars)
