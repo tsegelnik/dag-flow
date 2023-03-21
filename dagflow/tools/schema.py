ab5c203175966d5a1e84082c1dc587f259b020b3
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-21 17:26:27 +0300

    Squashed 'subtrees/dagflow/' changes from e8a89804..c4b1b128
    c4b1b128 feat: LoadFileWithExt, optionally update the dictionary
    
    git-subtree-dir: subtrees/dagflow
    git-subtree-split: c4b1b1285e1926211ab7fdb00c54a3dce374e1dd

--- dagflow/tools/schema.py
+++ dagflow/tools/schema.py
@@ -15,14 +15,20 @@ def IsFilewithExt(*exts: str):
         return any(filename.endswith(f'.{ext}' for ext in exts))
     return checkfilename
 
-def LoadFileWithExt(*, key: str=None,**kwargs: Callable):
+def LoadFileWithExt(*, key: Union[str, dict]=None, update: bool=False, **kwargs: Callable):
     """Returns a function that retunts True if the file extension is consistent"""
     def checkfilename(filename: Union[str, dict]):
         if key is not None:
-            filename = filename[key]
+            dct = filename.copy()
+            filename = dct.pop(key)
         for ext, loader in kwargs.items():
             if filename.endswith(f'.{ext}'):
-                return loader(filename)
+                ret = loader(filename)
+
+                if update:
+                    ret.update(dct)
+
+                return ret
 
             return False
     return checkfilename
