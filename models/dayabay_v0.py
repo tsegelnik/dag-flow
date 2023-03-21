f6102ba38204c208d84189e13caf2754bb074762
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-21 17:26:16 +0300

    chore: pprint

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -1,7 +1,10 @@
 from dagflow.bundles.load_variables import load_variables
 from pathlib import Path
 
+from pprint import pprint
+
 def model_dayabay_v0():
 	datasource = Path('data/dayabay-v0')
 	vars = load_variables({'load': str(datasource/'parameters/pdg2012.yaml')})
-	print(vars)
+
+	pprint(vars.object)
