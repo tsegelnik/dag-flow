8042382a2336f1d634b5882383b35fe23d6ccc09
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-22 16:18:11 +0300

    model: storage

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -1,12 +1,14 @@
 from dagflow.bundles.load_parameters import load_parameters
+from dictwrapper.dictwrapper import DictWrapper
 from pathlib import Path
 
 from pprint import pprint
 
 def model_dayabay_v0():
+	storage = DictWrapper({}, sep='.')
 	datasource = Path('data/dayabay-v0')
-	vars1 = load_parameters({'path': 'ibd', 'load': datasource/'parameters/pdg2012.yaml'})
-	vars2 = load_parameters({'path': 'reactor', 'load': datasource/'parameters/reactor_e_per_fission.yaml'})
+	storage |= load_parameters({'path': 'ibd', 'load': datasource/'parameters/pdg2012.yaml'})
+	storage |= load_parameters({'path': 'reactor', 'load': datasource/'parameters/reactor_thermal_power_nominal.yaml'})
+	storage |= load_parameters({'path': 'reactor', 'load': datasource/'parameters/detector_eres.yaml'})
 
-	pprint(vars1.object)
-	pprint(vars2.object)
+	pprint(storage.object)
