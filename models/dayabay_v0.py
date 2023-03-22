a165df2cc0c223c121f50f94659565826535e019
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-22 16:57:39 +0300

    model: nprotons

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -2,13 +2,13 @@ from dagflow.bundles.load_parameters import load_parameters
 from dictwrapper.dictwrapper import DictWrapper
 from pathlib import Path
 
-from pprint import pprint
-
 def model_dayabay_v0():
 	storage = DictWrapper({}, sep='.')
 	datasource = Path('data/dayabay-v0')
 	storage |= load_parameters({'path': 'ibd', 'load': datasource/'parameters/pdg2012.yaml'})
+	storage |= load_parameters({'path': 'detector', 'load': datasource/'parameters/detector_nprotons_correction.yaml'})
 	storage |= load_parameters({'path': 'reactor', 'load': datasource/'parameters/reactor_thermal_power_nominal.yaml'})
 	storage |= load_parameters({'path': 'reactor', 'load': datasource/'parameters/detector_eres.yaml'})
 
-	pprint(storage.object)
+	from pprint import pprint
+	pprint(storage.object, sort_dicts=False)
