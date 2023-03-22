49d95257a059509e6fb7a9d554e112e2f8eac7ce
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-22 16:56:51 +0300

    chore: clean logger

--- dagflow/logger.py
+++ dagflow/logger.py
@@ -12,19 +12,18 @@ from typing import Optional
 # To avoid a creation of duplicates save an instance
 _loggers = {}
 
-
 def get_logger(
-    filename: Optional[str] = None, debug: bool = False, **kwargs
+    name="dagflow",
+    *,
+    filename: Optional[str] = None,
+    debug: bool = False,
+    console: bool = False,
+    formatstr: Optional[str] = "%(asctime)s - %(levelname)s - %(message)s",
 ) -> Logger:
-    name = kwargs.pop("name", "PyGNA")
     if logger := _loggers.get(name):
         return logger
-    logger = getLogger("PyGNA")
-    formatstr = (
-        fmtstr
-        if (fmtstr := kwargs.pop("formatstr", False))
-        else "%(asctime)s - %(levelname)s - %(message)s"
-    )
+    logger = getLogger(name)
+
     level = DEBUG if debug else INFO
     logger.setLevel(level)
     formatter = Formatter(formatstr)
@@ -33,10 +32,14 @@ def get_logger(
         fh.setLevel(level)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
-    if kwargs.pop("console", True):
+    if console:
         ch = StreamHandler()
         ch.setLevel(level)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
     _loggers[name] = logger
     return logger
+
+logger = get_logger()
+SUBINFO    = INFO-10
+SUBSUBINFO = INFO-20
