2ae857fe3eb74f1383dd5ea8571868d40e14d5c6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-22 14:37:06 +0300

    chore: naming

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -1,12 +1,12 @@
-from dagflow.bundles.load_variables import load_variables
+from dagflow.bundles.load_parameters import load_parameters
 from pathlib import Path
 
 from pprint import pprint
 
 def model_dayabay_v0():
 	datasource = Path('data/dayabay-v0')
-	vars1 = load_variables({'path': 'ibd', 'load': datasource/'parameters/pdg2012.yaml'})
-	vars2 = load_variables({'path': 'reactor', 'load': datasource/'parameters/reactor_e_per_fission.yaml'})
+	vars1 = load_parameters({'path': 'ibd', 'load': datasource/'parameters/pdg2012.yaml'})
+	vars2 = load_parameters({'path': 'reactor', 'load': datasource/'parameters/reactor_e_per_fission.yaml'})
 
 	pprint(vars1.object)
 	pprint(vars2.object)
