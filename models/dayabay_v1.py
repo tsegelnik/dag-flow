be24a519630509a0734a180a45c16b2aef754d20
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-23 09:30:42 +0300

    test: fix tests

--- models/dayabay_v1.py
+++ models/dayabay_v1.py
@@ -1,10 +1,10 @@
-from dagflow.bundles.load_variables import load_variables
+from dagflow.bundles.load_parameters import load_parameters
 from pathlib import Path
 
 from pprint import pprint
 
 def model_dayabay_v1():
 	datasource = Path('data/dayabay-v1')
-	vars = load_variables({'path': 'ibd', 'load': datasource/'parameters/pdg2020.yaml'})
+	vars = load_parameters({'path': 'ibd', 'load': datasource/'parameters/pdg2020.yaml'})
 
 	pprint(vars.object)
