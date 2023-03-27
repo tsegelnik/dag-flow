40d22a7e8c77e20289ed3e27c854aaa704547fc5
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-03-27 12:58:37 +0300

    feat: parameters printer (WIP)

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -1,9 +1,31 @@
 from dagflow.bundles.load_parameters import load_parameters
-from dictwrapper.dictwrapper import DictWrapper
+from multikeydict.nestedmkdict import NestedMKDict
 from pathlib import Path
 
+from pandas import DataFrame
+class ParametersWrapper(NestedMKDict):
+	def to_dict(self, **kwargs) -> list:
+		data = []
+		for k, v in self.walkitems():
+			k = '.'.join(k)
+			dct = v.to_dict(**kwargs)
+			dct['path'] = k
+			data.append(dct)
+
+		return data
+
+	def to_df(self, **kwargs) -> DataFrame:
+		dct = self.to_dict(**kwargs)
+		columns = ('path', 'value', 'label')
+		df = DataFrame(dct, columns=columns)
+		return df
+
+	def to_latex(self) -> str:
+		df = self.to_df(label_from='latex')
+		return df.to_latex(escape=False)
+
 def model_dayabay_v0():
-	storage = DictWrapper({}, sep='.')
+	storage = ParametersWrapper({}, sep='.')
 	datasource = Path('data/dayabay-v0')
 	storage |= load_parameters({'path': 'ibd', 'load': datasource/'parameters/pdg2012.yaml'})
 	storage |= load_parameters({'path': 'detector', 'load': datasource/'parameters/detector_nprotons_correction.yaml'})
@@ -12,3 +34,9 @@ def model_dayabay_v0():
 
 	from pprint import pprint
 	pprint(storage.object, sort_dicts=False)
+
+	df = storage['constants'].to_df()
+	print(df)
+
+	tex = storage['constants'].to_latex()
+	print(tex)
