0a7039e4c83e4391eec26be50756ca79fb4680ba
Tsegelnik Nikita <tsegelnik@jinr.ru>
2023-05-03 16:17:37 +0300

    adding one check into Concatenation type function

--- dagflow/lib/Concatenation.py
+++ dagflow/lib/Concatenation.py
@@ -1,6 +1,10 @@
 from ..input_extra import MissingInputAddOne
 from ..nodes import FunctionNode
-from ..typefunctions import check_input_dimension, check_input_dtype
+from ..typefunctions import (
+    check_has_inputs,
+    check_input_dimension,
+    check_input_dtype,
+)
 
 
 class Concatenation(FunctionNode):
@@ -20,6 +24,7 @@ class Concatenation(FunctionNode):
 
     def _typefunc(self) -> None:
         """A output takes this function to determine the dtype and shape"""
+        check_has_inputs(self)
         cdtype = self.inputs[0].dd.dtype
         check_input_dtype(self, slice(None), cdtype)
         check_input_dimension(self, slice(None), 1)
