d320994448a12ed1eea99188b58f759ced451326
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-05-11 13:00:08 +0300

    feat: IBDXsecO1, add parameters, add type hinted numba function

--- reactornueosc/IBDXsecO1.py
+++ reactornueosc/IBDXsecO1.py
@@ -1,30 +1,42 @@
 from numpy.typing import ArrayLike
 from numpy import sin
-from numba import njit
+from numba import njit, void, float64
 
 from dagflow.typefunctions import check_input_dtype
 from dagflow.input_extra import MissingInputAddPair
 from dagflow.nodes import FunctionNode
-
-def _ibdxsecO1(enu, costheta, result):
-    pass
+from dagflow.input import Input
+from dagflow.output import Output
 
 class IBDXsecO1(FunctionNode):
     """Inverse beta decay cross section by Vogel and Beacom"""
+    __slots__ = ('_enu', '_ctheta', '_result')
+
+    _enu: Input
+    _ctheta: Input
+    _result: Output
 
     def __init__(self, name, *args, **kwargs):
         kwargs.setdefault("missing_input_handler", MissingInputAddPair())
         super().__init__(name, *args, **kwargs)
 
-        self.add_input('enu', positional=True, keyword=True)
-        # self.add_input('costheta', positional=True, keyword=True)
-        self.add_output('result', positional=True, keyword=True)
+        self._enu = self.add_input('enu', positional=True, keyword=True)
+        self._ctheta = self.add_input('costheta', positional=True, keyword=True)
+        self._result = self.add_output('result', positional=True, keyword=True)
 
-        self.add_input('ElectronMass', positional=False, keyword=True)
+        self._me = self.add_input('ElectronMass', positional=False, keyword=True)
+        self._mp = self.add_input('ProtonMass', positional=False, keyword=True)
+        self._mn = self.add_input('NeutronMass', positional=False, keyword=True)
 
     def _fcn(self, _, inputs, outputs):
-        for inp, out in zip(inputs, outputs):
-            _ibdxsecO1()
+        _ibdxsecO1(
+                self._enu.data.ravel(),
+                self._ctheta.data.ravel(),
+                self._result.data.ravel(),
+                self._me.data[0],
+                self._mp.data[0],
+                self._mn.data[0]
+                )
 
     def _typefunc(self) -> None:
         """A output takes this function to determine the dtype and shape"""
@@ -36,3 +48,6 @@ class IBDXsecO1(FunctionNode):
             out.dd.dtype = inp.dd.dtype
             out.dd.shape = inp.dd.shape
 
+@njit(void(float64[:], float64[:], float64[:], float64, float64, float64))
+def _ibdxsecO1(enu, costheta, result, me, mp, mn):
+    print("here", enu[0])
--- test/reactornuosc/test_IBDXsecO1.py
+++ test/reactornuosc/test_IBDXsecO1.py
@@ -6,21 +6,45 @@ from dagflow.lib.Array import Array
 from dagflow.lib.Integrator import Integrator
 from dagflow.lib.IntegratorSampler import IntegratorSampler
 from dagflow.lib.trigonometry import Cos, Sin
-from numpy import allclose, linspace, arange, meshgrid, pi, vectorize
+from dagflow.bundles.load_parameters import load_parameters
+from numpy import allclose, linspace, arange, meshgrid, pi, vectorize, meshgrid
 from pytest import mark, raises
 
 from reactornueosc.IBDXsecO1 import IBDXsecO1
 
 def test_IBDXsecO1(debug_graph, testname):
+    data = {
+            'format': 'value',
+            'state': 'fixed',
+            'parameters': {
+                'NeutronLifeTime': 879.4,        # s,   page 165
+                'NeutronMass':     939.565413,   # MeV, page 165
+                'ProtonMass':      938.272081,   # MeV, page 163
+                'ElectronMass':    0.5109989461, # MeV, page 16
+                },
+            'labels': {
+                'NeutronLifeTime': 'neutron lifetime, s (PDG2014)',
+                'NeutronMass': 'neutron mass, MeV (PDG2012)',
+                'ProtonMass': 'proton mass, MeV (PDG2012)',
+                'ElectronMass': 'electron mass, MeV (PDG2012)'
+                }
+            }
+    enu1 = linspace(1, 12.0, 111)
+    ctheta1 = linspace(-1, 1, 5)
+    enu2, ctheta2 = meshgrid(enu1, ctheta1, indexing='ij')
+
     with Graph(debug=debug_graph, close=True) as graph:
-        enu1 = linspace(1, 12.0, 111)
-        # costheta1 = linspace(-1, 1, )
-        enu = Array('enu', enu1)
-        me = Array('ElectronMass', [0.5])
+        storage = load_parameters(data)
+
+        enu = Array('enu', enu2)
+        ctheta = Array('ctheta', ctheta2)
         ibdxsec = IBDXsecO1('ibd')
 
-        enu >> ibdxsec
-        me >> ibdxsec.inputs["ElectronMass"]
+        (enu, ctheta) >> ibdxsec
+        ibdxsec << storage['parameter.constant']
+
+    csc = ibdxsec.get_data()
+    print(csc)
 
     #     npoints = 10
     #     edges = Array("edges", linspace(0, pi, npoints + 1))
