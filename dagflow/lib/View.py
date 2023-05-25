c56d6dce19a023b7d183f3874fa7f9b4b95e7ffb
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-05-25 15:50:42 +0300

    feat: View, support inputs passed as arguments

--- dagflow/lib/View.py
+++ dagflow/lib/View.py
@@ -1,17 +1,30 @@
+from typing import TYPE_CHECKING, Optional
+
 from ..nodes import FunctionNode
 from ..typefunctions import copy_from_input_to_output
+if TYPE_CHECKING:
+    from ..output import Output
+    from ..input import Input
+
 class View(FunctionNode):
     """Creates a node with a single data output which is a view on the input"""
+    __slots__ = ('_input',)
+    _input: "Input"
 
-    def __init__(self, name, outname="view", **kwargs):
+    def __init__(self, name, output: Optional["Output"]=None, *, outname="view", **kwargs):
         super().__init__(name, **kwargs)
-        output = self._add_output(
+        child_output = self._add_output(
             outname, allocatable=False, forbid_reallocation=True
         )
-        self._add_input("input", child_output=output)
+        self._input = self._add_input("input", child_output=child_output)
+
+        if output is not None:
+            output >> self._input
+            if output.closed:
+                self.close()
 
     def _fcn(self, _, inputs, outputs):
-        return self.inputs[0].data
+        return self._input.data
 
     def _typefunc(self) -> None:
         """A output takes this function to determine the dtype and shape"""
