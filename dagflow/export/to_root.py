1cedf45f87a73b1697190e60f11f0d109bfa6806
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-05-30 13:00:05 +0300

    feat: finalize to_root

--- dagflow/export/to_root.py
+++ dagflow/export/to_root.py
@@ -1,22 +1,25 @@
 from multikeydict.visitor import NestedMKDictVisitor
+from ..logger import logger, DEBUG
+from ..output import Output
 
-from numpy import allclose, frombuffer, dtype
+from numpy import allclose, frombuffer, dtype, array
 
 from typing import List, Dict, Union, Any, Tuple, TYPE_CHECKING
 from numpy.typing import NDArray
 
+from ROOT import TFile, TH1D, TH2D, TGraph, TGraph2D
+
 if TYPE_CHECKING:
     from pathlib import Path
-    from ..output import Output
 
     try:
-        from ROOT import TFile, TDirectory, TObject, TH1, TH2
+        from ROOT import TDirectory, TObject, TH1, TH2
     except ImportError:
         pass
 
-def to_root(output: Union['Output', Any]) -> Dict[str, 'TObject']:
+def to_root(output: Union[Output, Any]) -> Dict[str, 'TObject']:
     rets = {}
-    if not isinstance(output, 'Output'):
+    if not isinstance(output, Output):
         return rets
 
     dd = output.dd
@@ -36,16 +39,21 @@ def to_root(output: Union['Output', Any]) -> Dict[str, 'TObject']:
 
     return rets
 
+def _buffer_clean(buf: NDArray) -> NDArray:
+    # Should use ascontiguousarray, but it does not work properly for 'f'
+    return array(buf, dtype='d')
+
 def edges_to_args(edges: NDArray, *, rtol: float=1.e-9) -> Tuple:
+    edges = _buffer_clean(edges)
     widths = edges[1:]-edges[:-1]
     if allclose(widths, widths.max(), rtol=rtol, atol=0):
         return edges.size-1, edges[0], edges[-1]
 
-    return edges.size-1, edges.astype('d', copy=False)
+    return edges.size-1, edges
 
 def get_buffer_hist1(hist: 'TH1') -> NDArray:
     buf = hist.GetArray()
-    buf = frombuffer(buf, dtype(buf.typecode), h.GetNbinsX()+2)
+    buf = frombuffer(buf, dtype(buf.typecode), hist.GetNbinsX()+2)
     return buf[1:-1]
 
 def get_buffer_hist2(hist: 'TH2') -> NDArray:
@@ -53,74 +61,109 @@ def get_buffer_hist2(hist: 'TH2') -> NDArray:
     buf = hist.GetArray()
     res = frombuffer(buf, dtype(buf.typecode), (nx+2)*(ny+2)).reshape((ny+2, nx+2))
     # TODO: check order
-    return res[1:ny+1, 1:nx+1]
+    return res[1:ny+1, 1:nx+1].T
 
 def to_TH1(output: "Output") -> "TH1D":
-    from ROOT import TH1D
-
-    data = output.data
+    data = _buffer_clean(output.data)
     labels = output.labels
 
-    edges = edges_to_args(output.dd.axes_edges[0])
+    edges = edges_to_args(output.dd.axes_edges[0].data)
     hist = TH1D("", labels.roottitle, *edges)
     buffer = get_buffer_hist1(hist)
 
     buffer[:] = data
     hist.SetEntries(data.sum())
-    hist.SetXTitle(output.dd.axis_label(0, 'edges', root=True),)
+    hist.SetXTitle(output.dd.axes_edges[0].labels.rootaxis)
     hist.SetYTitle(labels.rootaxis)
 
     return hist
 
 def to_TH2(output: "Output") -> "TH2D":
-    from ROOT import TH2D
-
-    data = output.data
+    data = _buffer_clean(output.data)
     labels = output.labels
 
-    edgesX = edges_to_args(output.dd.axes_edges[0])
-    edgesY = edges_to_args(output.dd.axes_edges[1])
-    hist = TH1D("", labels.roottitle, *(edgesX+edgesY))
+    edgesX = edges_to_args(output.dd.axes_edges[0].data)
+    edgesY = edges_to_args(output.dd.axes_edges[1].data)
+    hist = TH2D("", labels.roottitle, *(edgesX+edgesY))
     buffer = get_buffer_hist2(hist)
 
     buffer[:] = data
     hist.SetEntries(data.sum())
-    hist.SetXTitle(output.dd.axis_label(0, 'edges', root=True))
-    hist.SetYTitle(output.dd.axis_label(1, 'edges', root=True))
+    hist.SetXTitle(output.dd.axes_edges[0].labels.rootaxis)
+    hist.SetYTitle(output.dd.axes_edges[1].labels.rootaxis)
     hist.SetZTitle(labels.rootaxis)
 
     return hist
 
 def to_TGraph(output):
-    pass
+    labels = output.labels
+
+    x = _buffer_clean(output.dd.axes_meshes[0].data)
+    y = _buffer_clean(output.data)
+
+    title = labels.roottitle
+    xtitle = output.dd.axes_meshes[0].labels.rootaxis
+    ytitle = labels.rootaxis
+
+    graph = TGraph(x.size, x, y)
+    graph.SetTitle(f'{title};{xtitle};{ytitle}')
+
+    return graph
 
 def to_TGraph2(output):
-    pass
+    labels = output.labels
+
+    x = _buffer_clean(output.dd.axes_meshes[0].data)
+    y = _buffer_clean(output.dd.axes_meshes[1].data)
+    z = _buffer_clean(output.data)
+
+    title = labels.roottitle
+    xtitle = output.dd.axes_meshes[0].labels.rootaxis
+    ytitle = output.dd.axes_meshes[1].labels.rootaxis
+    ztitle = labels.rootaxis
+
+    graph = TGraph2D(x.size, x.ravel(), y.ravel(), z.ravel())
+    graph.SetTitle(f'{title};{xtitle};{ytitle};{ztitle}')
+
+    return graph
 
 class ExportToRootVisitor(NestedMKDictVisitor):
-    __slots__ = ('_file', '_cwd', '_prevd')
+    __slots__ = ('_file', '_cwd', '_prevd', '_level')
     _file: 'TFile'
     _prevd: List["TDirectory"]
     _cwd: "TDirectory"
+    _level: int
 
-    def __init__(self, filename: Union[Path,str]):
+    def __init__(self, filename: Union['Path',str]):
         from ROOT import TFile
-        self._file = TFile(str(filename))
+        filename = str(filename)
+        logger.log(DEBUG, f"Create {filename}")
+        self._file = TFile(filename, 'RECREATE')
+        self._file.AddDirectory(False)
         if self._file.IsZombie():
             raise RuntimeError(f'File {filename!s} is zombie')
         self._prevd = []
         self._cwd = self._file
+        self._level = 0
 
     def start(self, dct):
         pass
 
     def enterdict(self, key, v):
-        path = '/'.join(key)
-        self._cwd = self._cwd.mkdir(path)
+        if not key:
+            return
+
+        self._level += 1
+
+        cwd = self._file
+        for subkey in key:
+            cwd = cwd.mkdir(subkey, "", True)
+
         self._prevd.append(self._cwd)
+        self._cwd = cwd
 
     def visit(self, key, value):
-        path = '/'.join(key)
+        path = '/'.join(key[self._level:])
 
         objects = to_root(value)
         if not objects:
@@ -140,8 +183,13 @@ class ExportToRootVisitor(NestedMKDictVisitor):
             raise RuntimeError(f'Unsupported ROOT objects: {objects}')
 
     def exitdict(self, k, v):
+        if not self._prevd:
+            return
+
+        self._level -= 1
         self._cwd = self._prevd.pop()
 
     def stop(self, dct):
-        pass
+        logger.log(DEBUG, f"Close {self._file.GetName()}")
+        self._file.Close()
 
