e7b2e51ec4ce5a7356f8978e3c215113984cdb46
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-05-31 16:19:10 +0300

    chore: refactor IntegratorGroup

--- dagflow/lib/IntegratorGroup.py
+++ dagflow/lib/IntegratorGroup.py
@@ -1,4 +1,10 @@
-from typing import Mapping
+from .Integrator import Integrator
+from .IntegratorSampler import IntegratorSampler
+from ..meta_node import MetaNode
+
+from typing import Mapping, TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..node import Node
 
 from ..meta_node import MetaNode
 from .Integrator import Integrator
@@ -6,30 +12,32 @@ from .IntegratorSampler import IntegratorSampler
 
 
 class IntegratorGroup(MetaNode):
-    def __init__(self, mode: str, dropdim: bool = True, *, labels: Mapping = {}):
+    __slots__ = ("_sampler")
+
+    _sampler: "Node"
+    def __init__(self, mode: str, *, dropdim: bool=True, labels: Mapping={}):
         super().__init__()
+        self._init_sampler(mode, "sampler", labels.get("sampler", {}))
+        self._add_integrator("integrator", labels.get("integrator", {}), dropdim=dropdim)
 
-        sampler = IntegratorSampler(
-            "sampler",
-            mode=mode,
-            label=labels.get("sampler", {}),
-        )
-        integrator = Integrator(
-            "integrator", dropdim=dropdim, label=labels.get("integrator", {})
-        )
-        sampler.outputs["weights"] >> integrator("weights")
-
-        if mode == "2d":
-            integrator("ordersY")
+    def _init_sampler(self, mode: str, name: str="sampler", label: Mapping={}):
+        self._sampler = IntegratorSampler(name, mode=mode, label=label)
 
         self._add_node(
-            sampler,
+            self._sampler,
             kw_inputs=["ordersX"],
             kw_inputs_optional=["ordersY"],
             kw_outputs=["x"],
             kw_outputs_optional=["y"],
-            merge_inputs=["ordersX", "ordersY"],
+            merge_inputs=["ordersX", "ordersY"]
         )
+
+    def _add_integrator(self, name: str="integrator", label: Mapping={}, *, dropdim: bool):
+        integrator = Integrator(name, dropdim=dropdim, label=label)
+        if self._sampler.mode=="2d":
+            integrator("ordersY")
+        self._sampler.outputs["weights"] >> integrator("weights")
+
         self._add_node(
             integrator,
             kw_inputs=["ordersX"],
