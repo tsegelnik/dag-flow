0870884d5f7a979545f93eb5d58e8f176be39500
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-05-31 17:33:45 +0300

    feat, draft: `IntegratorGroup.replicate` with storage support

--- dagflow/lib/IntegratorGroup.py
+++ dagflow/lib/IntegratorGroup.py
@@ -1,8 +1,9 @@
 from .Integrator import Integrator
 from .IntegratorSampler import IntegratorSampler
 from ..meta_node import MetaNode
+from ..storage import NodeStorage
 
-from typing import Mapping, TYPE_CHECKING
+from typing import Mapping, TYPE_CHECKING, Tuple, Union
 if TYPE_CHECKING:
     from ..node import Node
 
@@ -35,7 +36,14 @@ class IntegratorGroup(MetaNode):
             merge_inputs=["ordersX", "ordersY"]
         )
 
-    def _add_integrator(self, name: str="integrator", label: Mapping={}, *, dropdim: bool):
+    def _add_integrator(
+        self,
+        name: str="integrator",
+        label: Mapping={},
+        *,
+        positionals: bool=True,
+        dropdim: bool
+    ) -> Integrator:
         integrator = Integrator(name, dropdim=dropdim, label=label)
         if self._sampler.mode=="2d":
             integrator("ordersY")
@@ -46,17 +54,36 @@ class IntegratorGroup(MetaNode):
             kw_inputs=["ordersX"],
             kw_inputs_optional=["ordersY"],
             merge_inputs=["ordersX", "ordersY"],
-            inputs_pos=True,
-            outputs_pos=True,
+            inputs_pos=positionals,
+            outputs_pos=positionals,
             missing_inputs=True,
             also_missing_outputs=True,
         )
 
+        return integrator
+
     @classmethod
-    def replicate(cls, mode: str, labels: Mapping={}, **kwargs):
-        integrators = cls(mode, bare=True, **kwargs)
+    def replicate(
+        cls,
+        mode: str,
+        labels: Mapping={},
+        *,
+        replicate: Tuple[Union[Tuple[str,...], str],...]=((),),
+        dropdim: bool=True
+    ) -> "IntegratorGroup":
+        integrators = cls(mode, bare=True)
+        storage = NodeStorage({'nodes': {}, 'outputs': {}})
 
         integrators._init_sampler(mode, "sampler", labels.get("sampler", {}))
-        integrators._add_integrator("integrator", labels.get("integrator", {}))
+        label_int = labels.get("integrator", {})
+        for key in replicate:
+            name = ".".join(("integrator",) + key)
+            integrator = integrators._add_integrator(name, label_int, positionals=False, dropdim=dropdim)
+            integrator()
+            storage(('nodes', 'kinint'))[key] = integrator
+            storage(('outputs', 'kinint'))[key] = integrator.outputs[0]
+
+        if (common_storage := NodeStorage.current()) is not None:
+            common_storage^=storage
 
         return integrators
