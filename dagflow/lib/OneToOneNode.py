7345d97ff60559effa606847ecd5fe781df5f951
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-06-02 17:19:32 +0300

    feat: OneToOneNode replicate

--- dagflow/lib/OneToOneNode.py
+++ dagflow/lib/OneToOneNode.py
@@ -1,5 +1,11 @@
 from ..input_extra import MissingInputAddPair
 from ..nodes import FunctionNode
+from ..node import Node
+from ..storage import NodeStorage
+
+from multikeydict.typing import KeyLike
+
+from typing import Tuple, Optional
 
 class OneToOneNode(FunctionNode):
     """
@@ -20,3 +26,30 @@ class OneToOneNode(FunctionNode):
         check_has_inputs(self)
         copy_from_input_to_output(self, slice(None), slice(None), edges=True, nodes=True)
         assign_outputs_axes_from_inputs(self, slice(None), slice(None), assign_meshes=True, ignore_assigned=True, ignore_Nd=True)
+
+    @classmethod
+    def replicate(
+        cls,
+        name: str,
+        replicate: Tuple[KeyLike,...]=((),),
+        **kwargs
+    ) -> Tuple[Optional[Node], NodeStorage]:
+        storage = NodeStorage(default_containers=True)
+        nodes = storage('nodes')
+        inputs = storage('inputs')
+        outputs = storage('outputs')
+
+        for outkey in replicate:
+            outname = (name,)+outkey
+            instance = cls('.'.join(outname), **kwargs)
+            nodes[outname] = instance
+            instance()
+            inputs[outname] = instance.inputs[0]
+            outputs[outname] = instance.outputs[0]
+
+        NodeStorage.update_current(storage, strict=True)
+
+        if len(outkey)==1:
+            return instance, storage
+
+        return None, storage
