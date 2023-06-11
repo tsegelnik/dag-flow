4734cdf70f107656dd790b7b2b6083020f2e1523
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-06-11 22:56:44 +0800

    feat: default folder 'stat'

--- dagflow/storage.py
+++ dagflow/storage.py
@@ -44,7 +44,7 @@ class NodeStorage(NestedMKDict):
 
         if not default_containers:
             return
-        for name in ("parameters", "nodes", "inputs", "outputs"):
+        for name in ("parameters", "stat", "nodes", "inputs", "outputs"):
             self.child(name)
 
     def plot(self, *args, **kwargs) -> None:
