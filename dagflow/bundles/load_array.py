dd1cd748609c056791f848f3216f85bfe79d9527
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-09-11 12:45:26 +0300

    feat: add load_array bundle
    Clean load_graph

---
+++ dagflow/bundles/load_array.py
@@ -0,0 +1,213 @@
+from pathlib import Path
+from typing import Mapping, Optional
+
+from numpy.typing import NDArray
+from schema import And
+from schema import Optional as SchemaOptional
+from schema import Or, Schema, Use
+
+from ..lib.Array import Array
+from ..storage import NodeStorage
+from ..tools.schema import (
+    AllFileswithExt,
+    IsFilenameSeqOrFilename,
+    IsStrSeqOrStr,
+    LoadFileWithExt,
+    LoadYaml,
+)
+
+_extensions = {"root", "hdf5", "tsv", "txt"}
+_schema_cfg = Schema(
+    {
+        "name": str,
+        "filenames": And(IsFilenameSeqOrFilename, AllFileswithExt(*_extensions)),
+        SchemaOptional("replicate", default=((),)): Or(
+            (IsStrSeqOrStr,), [IsStrSeqOrStr]
+        ),
+        SchemaOptional("objects", default={}): {str: str},
+    }
+)
+
+_schema_loadable_cfg = And(
+    {"load": Or(str, And(Path, Use(str))), SchemaOptional(str): object},
+    Use(
+        LoadFileWithExt(yaml=LoadYaml, key="load", update=True),
+        error="Failed to load {}",
+    ),
+    _schema_cfg,
+)
+
+
+def _validate_cfg(cfg):
+    if isinstance(cfg, dict) and "load" in cfg:
+        return _schema_loadable_cfg.validate(cfg)
+    else:
+        return _schema_cfg.validate(cfg)
+
+
+def load_array(acfg: Optional[Mapping] = None, **kwargs):
+    acfg = dict(acfg or {}, **kwargs)
+    cfg = _validate_cfg(acfg)
+
+    name = (cfg["name"],)
+    filenames = cfg["filenames"]
+    keys = cfg["replicate"]
+    objects = cfg["objects"]
+
+    try:
+        ext = next(ext for ext in _extensions if filenames[0].endswith(f".{ext}"))
+    except StopIteration:
+        raise RuntimeError(f"Unable to find extension for: {filenames[0]}")
+
+    loader = _loaders.get(ext)
+    if loader is None:
+        raise RuntimeError(f"Unable to find loader for: {filenames[0]}")
+    match_filename = len(filenames) > 1
+
+    data = {}
+    for key in keys:
+        if match_filename:
+            try:
+                filename = next(
+                    filename
+                    for filename in filenames
+                    if all(subkey in filename for subkey in key)
+                )
+            except StopIteration:
+                raise RuntimeError(f"Unable to find a file for {key}")
+        else:
+            filename = filenames[0]
+
+        skey = ".".join(key)
+        iname = objects.get(skey, skey)
+        data[name + key] = loader(filename, iname)
+    storage = NodeStorage(default_containers=True)
+    with storage:
+        for key, array in data.items():
+            Array.make_stored(key, array)
+
+    NodeStorage.update_current(storage, strict=True)
+
+    return storage
+
+
+def _load_tsv(filename: str, _: str) -> NDArray:
+    from numpy import loadtxt
+
+    return loadtxt(filename)
+
+
+def _load_hdf5(filename: str, name: str) -> NDArray:
+    if not name:
+        raise RuntimeError(f"Need an object name to read from {filename}")
+    from h5py import File
+
+    file = File(filename, "r")
+    try:
+        data = file[name]
+    except KeyError as e:
+        raise RuntimeError(f"Unable to read {name} from {filename}") from e
+
+    return data[:]
+
+
+def _load_root_uproot(filename: str, name: str) -> NDArray:
+    if not name:
+        raise RuntimeError(f"Need an object name to read from {filename}")
+    from uproot import open
+
+    file = open(filename)
+    try:
+        data = file[name]
+    except KeyError as e:
+        raise RuntimeError(f"Unable to read {name} from {filename}") from e
+
+    return data.to_numpy()
+
+
+def _load_root_ROOT(filename: str, name: str) -> NDArray:
+    if not name:
+        raise RuntimeError(f"Need an object name to read from {filename}")
+    from ROOT import TFile
+
+    file = TFile(filename)
+    if file.IsZombie():
+        raise RuntimeError(f"Can not open file {filename}")
+    data = file.Get(name)
+    if not data:
+        raise RuntimeError(f"Unable to read {name} from {filename}") from e
+
+    return _get_buffer(data)
+
+
+def _load_root(filename: str, *args, **kwargs) -> NDArray:
+    try:
+        return _load_root_uproot(filename, *args, **kwargs)
+    except AttributeError:
+        pass
+
+    try:
+        return _load_root_ROOT(filename, *args, **kwargs)
+    except ImportError:
+        raise RuntimeError(
+            f"Error reading file {filename}. `uproot` is unable, `ROOT` is not found."
+        )
+
+
+_loaders = {
+    "txt": _load_tsv,
+    "tsv": _load_tsv,
+    "root": _load_root,
+    "hdf5": _load_hdf5,
+}
+
+from numpy import dtype, frombuffer
+
+
+def _get_buffer_hist1(h, flows=False):
+    """Return TH1* histogram data buffer
+    if flows=False, exclude underflow and overflow
+    """
+    buf = h.GetArray()
+    buf = frombuffer(buf, dtype(buf.typecode), h.GetNbinsX() + 2)
+    if not flows:
+        buf = buf[1:-1]
+    return buf.copy()
+
+
+def _get_buffer_hist2(h, flows=False):
+    """Return histogram data buffer
+    if flows=False, exclude underflow and overflow
+    NOTE: buf[biny][binx] is the right access signature
+    """
+    nx, ny = h.GetNbinsX(), h.GetNbinsY()
+    buf = h.GetArray()
+    res = frombuffer(buf, dtype(buf.typecode), (nx + 2) * (ny + 2)).reshape(
+        (ny + 2, nx + 2)
+    )
+    if not flows:
+        res = res[1 : ny + 1, 1 : nx + 1]
+
+    return res.copy()
+
+
+def _get_buffer_matrix(m):
+    """Get TMatrix buffer"""
+    cbuf = m.GetMatrixArray()
+    res = frombuffer(cbuf, dtype(cbuf.typecode), m.GetNoElements()).reshape(
+        m.GetNrows(), m.GetNcols()
+    )
+    return res.copy()
+
+
+def _get_buffer(obj):
+    import ROOT
+
+    if isinstance(obj, ROOT.TH1) and obj.GetDimension() == 1:
+        return _get_buffer_hist1(obj)
+    if isinstance(obj, ROOT.TH2) and obj.GetDimension() == 2:
+        return _get_buffer_hist2(obj)
+    if isinstance(obj, (ROOT.TMatrixD, ROOT.TMatrixF)):
+        return _get_buffer_matrix(obj)
+
+    raise Exception()
--- dagflow/bundles/load_graph.py
+++ dagflow/bundles/load_graph.py
@@ -1,51 +1,55 @@
-from typing import Optional, Mapping, Tuple
-from numpy.typing import NDArray
-from schema import Schema, Optional as SchemaOptional, And, Use, Or
 from pathlib import Path
-
-from ..tools.schema import (
-    IsStrSeqOrStr,
-    IsFilenameSeqOrFilename,
-    AllFileswithExt,
-    LoadFileWithExt,
-    LoadYaml
-)
-from ..storage import NodeStorage
-from ..lib.Array import Array
+from typing import Mapping, Optional, Tuple
 
 from numpy import allclose
+from numpy.typing import NDArray
+from schema import And
+from schema import Optional as SchemaOptional
+from schema import Or, Schema, Use
+
+from ..lib.Array import Array
+from ..storage import NodeStorage
+from ..tools.schema import (
+    AllFileswithExt,
+    IsFilenameSeqOrFilename,
+    IsStrSeqOrStr,
+    LoadFileWithExt,
+    LoadYaml,
+)
 
 _extensions = {"root", "hdf5", "tsv", "txt"}
-_schema_cfg = Schema({
-    "name": str,
-    "filenames": And(IsFilenameSeqOrFilename, AllFileswithExt(*_extensions)),
-    SchemaOptional("merge_x", default=False): bool,
-    SchemaOptional("x", default="x"): str,
-    SchemaOptional("y", default="y"): str,
-    SchemaOptional("replicate", default=((),)): Or((IsStrSeqOrStr,),[IsStrSeqOrStr]),
-    SchemaOptional("objects", default={}): {str: str},
-    })
+_schema_cfg = Schema(
+    {
+        "name": str,
+        "filenames": And(IsFilenameSeqOrFilename, AllFileswithExt(*_extensions)),
+        SchemaOptional("merge_x", default=False): bool,
+        SchemaOptional("x", default="x"): str,
+        SchemaOptional("y", default="y"): str,
+        SchemaOptional("replicate", default=((),)): Or(
+            (IsStrSeqOrStr,), [IsStrSeqOrStr]
+        ),
+        SchemaOptional("objects", default={}): {str: str},
+    }
+)
 
 _schema_loadable_cfg = And(
-            {
-                'load': Or(str, And(Path, Use(str))),
-                SchemaOptional(str): object
-            },
-            Use(LoadFileWithExt(
-                yaml=LoadYaml,
-                key='load',
-                update=True
-            ), error='Failed to load {}'),
-            _schema_cfg
-        )
+    {"load": Or(str, And(Path, Use(str))), SchemaOptional(str): object},
+    Use(
+        LoadFileWithExt(yaml=LoadYaml, key="load", update=True),
+        error="Failed to load {}",
+    ),
+    _schema_cfg,
+)
+
 
 def _validate_cfg(cfg):
-    if isinstance(cfg, dict) and 'load' in cfg:
+    if isinstance(cfg, dict) and "load" in cfg:
         return _schema_loadable_cfg.validate(cfg)
     else:
         return _schema_cfg.validate(cfg)
 
-def load_graph(acfg: Optional[Mapping]=None, **kwargs):
+
+def load_graph(acfg: Optional[Mapping] = None, **kwargs):
     acfg = dict(acfg or {}, **kwargs)
     cfg = _validate_cfg(acfg)
 
@@ -54,69 +58,76 @@ def load_graph(acfg: Optional[Mapping]=None, **kwargs):
     keys = cfg["replicate"]
     objects = cfg["objects"]
 
-    xname = cfg['x']
-    yname = cfg['y']
+    xname = name, cfg["x"]
+    yname = name, cfg["y"]
 
-    for ext in _extensions:
-        if filenames[0].endswith(f".{ext}"):
-            break
-    else:
-        raise RuntimeError(f"Unable to process extension for: {filenames[0]}")
+    try:
+        ext = next(ext for ext in _extensions if filenames[0].endswith(f".{ext}"))
+    except StopIteration:
+        raise RuntimeError(f"Unable to find extension for: {filenames[0]}")
 
     loader = _loaders.get(ext)
     if loader is None:
         raise RuntimeError(f"Unable to find loader for: {filenames[0]}")
-    match_filename = len(filenames)>1
+    match_filename = len(filenames) > 1
 
     meshes = []
     data = {}
     for key in keys:
         if match_filename:
-            for filename in filenames:
-                if all(subkey in filename for subkey in key): break
-            else:
+            try:
+                filename = next(
+                    filename
+                    for filename in filenames
+                    if all(subkey in filename for subkey in key)
+                )
+            except StopIteration:
                 raise RuntimeError(f"Unable to find a file for {key}")
         else:
             filename = filenames[0]
 
-        skey = '.'.join(key)
+        skey = ".".join(key)
         iname = objects.get(skey, skey)
         x, y = loader(filename, iname)
-        data[skey] = x, y
+        data[key] = x, y
         meshes.append(x)
 
     if cfg["merge_x"]:
         x0 = meshes[0]
         for xi in meshes[1:]:
             if not allclose(x0, xi, atol=0, rtol=0):
-                raise RuntimeError('load_graph: inconsistent x axes, unable to merge.')
+                raise RuntimeError("load_graph: inconsistent x axes, unable to merge.")
 
-        commonmesh, _ = Array.make_stored(f"{name}.{xname}", x0)
+        commonmesh, _ = Array.make_stored(".".join(xname), x0)
     else:
         commonmesh = None
 
     storage = NodeStorage(default_containers=True)
     with storage:
-        for key in keys:
-            skey = '.'.join(key)
-            x, y = data[skey]
+        for key, (x, y) in data.items():
             if commonmesh:
                 mesh = commonmesh
             else:
-                mesh, _ = Array.make_stored(f"{name}.{xname}.{skey}", x)
-            Array.make_stored(f"{name}.{yname}.{skey}", y, meshes=mesh)
+                xkey = ".".join(xname + key)
+                mesh, _ = Array.make_stored(xkey, x)
+            ykey = ".".join(yname + key)
+            Array.make_stored(ykey, y, meshes=mesh)
 
     NodeStorage.update_current(storage, strict=True)
 
     return storage
 
+
 def _load_tsv(filename: str, name: str) -> NDArray:
     from numpy import loadtxt
+
     return loadtxt(filename, unpack=True)
+
 
 def _load_hdf5(filename: str, name: str) -> Tuple[NDArray, NDArray]:
     from h5py import File
-    file = File(filename, 'r')
+
+    file = File(filename, "r")
     try:
         data = file[name]
     except KeyError as e:
@@ -125,8 +136,10 @@ def _load_hdf5(filename: str, name: str) -> Tuple[NDArray, NDArray]:
     cols = data.dtype.names
     return data[cols[0]], data[cols[1]]
 
+
 def _load_root(filename: str, name: str) -> Tuple[NDArray, NDArray]:
     from uproot import open
+
     file = open(filename)
     try:
         data = file[name]
@@ -137,9 +150,10 @@ def _load_root(filename: str, name: str) -> Tuple[NDArray, NDArray]:
 
     return x[:-1], y
 
+
 _loaders = {
-        'txt': _load_tsv,
-        'tsv': _load_tsv,
-        'root': _load_root,
-        'hdf5': _load_hdf5,
-        }
+    "txt": _load_tsv,
+    "tsv": _load_tsv,
+    "root": _load_root,
+    "hdf5": _load_hdf5,
+}
