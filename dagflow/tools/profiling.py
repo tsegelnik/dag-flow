e1ae31253c3f36956dce36852272e416d4c82dc3
ggeorge <577-ggeorge@users.noreply.git.jinr.ru>
2023-09-14 11:02:56 +0300

    feat(profiling): group profiling

--- dagflow/tools/profiling.py
+++ dagflow/tools/profiling.py
@@ -1,6 +1,11 @@
-from timeit import timeit
+from __future__ import annotations
+
+from timeit import timeit, repeat
 from functools import cached_property
-from typing import List
+import collections
+from typing import List, Set
+import numpy as np
+
 
 from ..nodes import FunctionNode
 
@@ -65,23 +70,26 @@ class Profiling:
         if not n_runs:
             n_runs = self._n_runs
 
-        # check node inputs 
-        for input in node.inputs:
-            if input.tainted:
-                input.touch()
-        # node.touch(force=True)
+        # compute and cache all inputs to prevent 
+        # calculations during time measurment
+        for input in node.inputs.iter_all():
+            input.touch()
 
-        testing_function = lambda : node._fcn(node, node.inputs, node.outputs)
-        estimations = timeit(stmt=testing_function, number=n_runs)
+        testing_function = lambda : node.fcn(node, node.inputs, node.outputs)
+        testing_function() # ignore the first calculation
+        # estimations = timeit(stmt=testing_function, number=n_runs)
+        estimations = repeat(stmt=testing_function, repeat=100, number=10000)
+        # print(estimations)
+        print(node.name, min(estimations), max(estimations))
 
-        result = EstimateRecord(node, n_runs, estimations)
+        result = EstimateRecord(node, n_runs, min(estimations))
 
         self._results.append(result)
         return result
     
     def estimate_graph(self, graph):
         for node in graph._nodes:
-            self.estimate_node(node, n_runs=10000)
+            self.estimate_node(node, n_runs=self._n_runs)
 
         return self
     
@@ -103,3 +111,104 @@ class Profiling:
                                  record.avg_time,
                                  record.n_runs))
         
+class GroupProfiling:
+
+    _estimations: List[EstimateRecord]
+    _excluded_nodes: List[FunctionNode]
+    _removed_fcns: collections.deque[FunctionNode]
+    _n_runs: int
+
+    # slots
+
+    # список узлов, которые нас интересуют
+
+    # del node.fcn
+
+    # _stash_ _unwrap_
+
+    # make base class   
+
+    def __init__(self, 
+                 excluded_nodes: List[FunctionNode] = [],
+                 n_runs = 1) -> None:
+        self._estimations = list()
+        self._excluded_nodes = excluded_nodes
+        self._n_runs = n_runs
+        self._removed_fcns = collections.deque()
+
+    def taint_parents(self, node): 
+        if node not in self._excluded_nodes:
+            for input in node.inputs.iter_all():
+                    self.taint_parents(input.parent_node)
+
+            node.taint()    
+
+    # using only `node` argument for further compatibility
+    @staticmethod
+    def fcn_no_computation(node: FunctionNode, inputs, outputs):
+        for input in node.inputs.iter_all():
+            input.touch()
+
+        # print(node.name)
+        
+        # Note: may work much slower than previos fcn() for nodes 
+        # where return is just a number and etc
+        # return list(node.outputs.iter_data())
+        # return None
+        
+
+    def make_fcns_empty(self, node: FunctionNode): 
+        if node not in self._excluded_nodes:
+            for input in node.inputs.iter_all():
+                self.make_fcns_empty(input.parent_node)
+
+            self._removed_fcns.append(node.fcn)
+            node.fcn = self.fcn_no_computation
+
+    def restore_fcns(self, node: FunctionNode):
+        if node not in self._excluded_nodes:
+            for input in node.inputs.iter_all():
+                self.restore_fcns(input.parent_node)
+
+            node.fcn = self._removed_fcns.popleft()
+
+    def estimate_group_with_empty_fcn(self, head_node: FunctionNode):
+        self.make_fcns_empty(head_node)
+        setup = lambda: self.taint_parents(head_node)
+
+        results = np.array(repeat(stmt=head_node.eval, setup=setup, repeat=500, number=1))
+
+        print("\tMean:", results.mean())
+        print("\tStd:", results.std())
+        print("\tMin:", results.min())
+
+        self.restore_fcns(head_node)
+
+
+    # def estimate_group_with_normal_fcn(self, head_node: FunctionNode):
+    #     self.taint_parents(head_node)
+        
+
+
+
+    # def estimate_group(self,    
+    #                    head_node: FunctionNode, 
+    #                    excluded_nodes: List[FunctionNode]):
+    #     tree_stack = collections.deque()
+    #     tree_stack.append(head_node)
+    #     cur_node = head_node
+    #     while tree_stack.count():
+    #         for input in cur_node.inputs.iter_all():
+    #         for head_node.inputs.iter_all()
+    #         cur_node = head_node
+    #         for input in head_node.inputs.iter_all():
+        
+        # полный обход дерева
+        # вычисление fcn для каждого 
+        # вычисление 1 раз? exept
+        # вычисление времени когда все родительские taint, посчитать разницу
+        # подумать как хранить данные
+        
+
+
+
--- tests/test_profiling.py
+++ tests/test_profiling.py
@@ -2,29 +2,35 @@
 #       pytest -s ./test/test_profiling.py
 import numpy as np
 
-from dagflow.tools.profiling import Profiling
+from dagflow.tools.profiling import Profiling, GroupProfiling
 
 from dagflow.graph import Graph
 from dagflow.lib.Array import Array
+from dagflow.lib.ElSumSq import ElSumSq
+# from dagflow.lib import ElSumSq
 from dagflow.lib.MatrixProductDVDt import MatrixProductDVDt
 
 left = np.array([[1, 2, 3], [3, 4, 5], [3, 4, 5]], dtype='d')
+left2 = np.array([[1, 2, 3], [3, 4, 5], [3, 4, 5]], dtype='d')
 square = np.diag(np.array([9, 4, 5], dtype='d'))
 square2 = np.diag(np.array([9, 4, 5], dtype='d'))
 
+rng = np.random.default_rng()
+
+arrays_data = rng.random(size=(10, 20), dtype='float64')
+print(arrays_data[0])
+
+
 with Graph(close=True) as graph:
-    l_array = Array("Left", left)
-    s_array = Array("Square", square)
-    s2_array = Array("Square2", square2)
-
-    prod = MatrixProductDVDt("MatrixProductDVDt2d")
-    l_array >> prod.inputs["left"]
-    s_array >> prod.inputs["square"]
-
-    prod2 = MatrixProductDVDt("MatrixProductDVDt2d2")
-    l_array >> prod2.inputs["left"]
-    s2_array >> prod2.inputs["square"]
+    array_nodes = [Array(f"A_{i}", arrays_data[i]) for i in range(10)]
+    elsum = ElSumSq("ElSumSq")
+    array_nodes >> elsum
 
 
-profiling = Profiling().estimate_graph(graph)
+print(list(elsum.outputs.iter_data()))
+profiling = Profiling(n_runs=10000).estimate_graph(graph)
 profiling.make_report()
+
+profiling = GroupProfiling()
+profiling.estimate_group_with_empty_fcn()
+
