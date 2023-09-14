bf164797643f9b3cd8d6abec93d56bc1c45dc53c
ggeorge <577-ggeorge@users.noreply.git.jinr.ru>
2023-09-14 11:51:09 +0300

    refactor: extend convenient imports to all nodes

--- dagflow/lib/__init__.py
+++ dagflow/lib/__init__.py
@@ -2,3 +2,25 @@ from .arithmetic import Sum, Product, Division
 from .trigonometry import Cos, Sin, Tan, ArcCos, ArcSin, ArcTan
 from .Array import Array
 from .WeightedSum import WeightedSum
+from .Cholesky import Cholesky
+from .Concatenation import Concatenation
+from .CovmatrixFromCormatrix import CovmatrixFromCormatrix
+from .Dummy import Dummy
+from .ElSumSq import ElSumSq
+from .Integrator import Integrator
+from .IntegratorGroup import IntegratorGroup
+from .IntegratorSampler import IntegratorSampler
+from .Interpolator import Interpolator
+from .InterpolatorGroup import InterpolatorGroup
+from .ManyToOneNode import ManyToOneNode
+from .MatrixProductAB import MatrixProductAB
+from .NormalizeCorrelatedVars import NormalizeCorrelatedVars
+from .NormalizeCorrelatedVars2 import NormalizeCorrelatedVars2
+from .OneToOneNode import OneToOneNode
+from .PartialSums import PartialSums
+from .SegmentIndex import SegmentIndex
+from .SumMatOrDiag import SumMatOrDiag
+from .SumSq import SumSq
+from .VectorMatrixProduct import VectorMatrixProduct
+from .View import View
+from .ViewConcat import ViewConcat
