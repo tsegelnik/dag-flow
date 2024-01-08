1f3b09744458fa29e40f97a4a8a8674e7689ce7e
Tsegelnik Nikita <tsegelnik@jinr.ru>
2024-01-09 01:11:30 +0300

    fix diag, add offdiag, and update the test

---
+++ dagflow/lib/RenormalizeDiag.py
@@ -0,0 +1,115 @@
+from typing import TYPE_CHECKING, Literal
+
+from numba import njit
+from numpy.typing import NDArray
+
+from ..inputhandler import MissingInputAddPair
+from ..typefunctions import (
+    AllPositionals,
+    check_input_shape,
+    check_input_square,
+    check_inputs_equivalence,
+)
+from .OneToOneNode import OneToOneNode
+
+if TYPE_CHECKING:
+    from ..input import Input
+
+
+class RenormalizeDiag(OneToOneNode):
+    __slots__ = ("_mode", "_ndiag", "_scale")
+
+    _mode: str
+    _ndiag: int
+    _scale: "Input"
+
+    def __init__(
+        self, *args, mode: Literal["diag", "offdiag"] = "diag", ndiag: int = 1, **kwargs
+    ) -> None:
+        super().__init__(
+            *args,
+            missing_input_handler=MissingInputAddPair(input_fmt="matrix", output_fmt="result"),
+            **kwargs,
+        )
+
+        if mode in {"diag", "offdiag"}:
+            self._labels.setdefault("mark", f"renormalize {mode}")
+        else:
+            raise RuntimeError(f"Invalid RenormalizeDiag mode {mode}")
+        self._mode = mode
+
+        if ndiag < 1:
+            raise RuntimeError(f"Invalid RenormalizeDiag ndiag={ndiag} (<1)")
+        self._ndiag = ndiag
+
+        self._scale = self._add_input("scale", positional=False)
+        self._functions.update({"diag": self._fcn_diag, "offdiag": self._fcn_offdiag})
+
+    def _fcn_diag(self) -> None:
+        scale = self._scale.data[0]
+        for input, output in zip(self.inputs, self.outputs):
+            _renorm_diag_python(input.data, output._data, scale, self._ndiag)
+
+    def _fcn_offdiag(self) -> None:
+        scale = self._scale.data[0]
+        for input, output in zip(self.inputs, self.outputs):
+            _renorm_offdiag_python(input.data, output._data, scale, self._ndiag)
+
+    def _typefunc(self) -> None:
+        super()._typefunc()
+        check_input_shape(self, "scale", (1,))
+        check_input_square(self, 0)
+        check_inputs_equivalence(self, AllPositionals, check_dtype=True, check_shape=True)
+        self.fcn = self._functions[self._mode]
+
+
+@njit(cache=True)
+def zero_to_one(x: float) -> float:
+    return x if x != 0.0 else 1.0
+
+
+def _renorm_diag_python(matrix: NDArray, out: NDArray, scale: float, ndiag: float) -> None:
+    out[:, :] = matrix[:, :]
+    n = out.shape[0]
+    # main diag
+    for i in range(n):
+        out[i, i] *= scale
+    # other diagonals
+    idiag = 1
+    while idiag < ndiag:
+        i = 0
+        while i + idiag < n:
+            out[i + idiag, i] *= scale
+            out[i, i + idiag] *= scale
+            i += 1
+        idiag += 1
+    # norming
+    out[:, :] = [out[irow, :] / zero_to_one(out[:, irow].sum()) for irow in range(n)]
+
+
+def _renorm_offdiag_python(matrix: NDArray, out: NDArray, scale: float, ndiag: float) -> None:
+    out[:, :] = matrix[:, :] * scale
+    n = out.shape[0]
+    # main diag
+    for i in range(n):
+        out[i, i] = matrix[i, i]
+    # other diagonals
+    idiag = 1
+    while idiag < ndiag:
+        i = 0
+        while i + idiag < n:
+            out[i + idiag, i] = matrix[i + idiag, i]
+            out[i, i + idiag] = matrix[i, i + idiag]
+            i += 1
+        idiag += 1
+    # norming
+    out[:, :] = [out[irow, :] / zero_to_one(out[:, irow].sum()) for irow in range(n)]
+
+
+# NOTE: to implement numba-methods solve the problem with comprehensions
+# _renorm_diag_numba: Callable[[NDArray, NDArray, float, float], None] = njit(cache=True)(
+#    _renorm_diag_python
+# )
+# _renorm_offdiag_numba: Callable[[NDArray, NDArray, float, float], None] = njit(cache=True)(
+#    _renorm_offdiag_python
+# )
---
+++ test/nodes/test_RenormalizeDiag.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from numpy import allclose, finfo, ones
+from pytest import mark
+
+from dagflow.graph import Graph
+from dagflow.graphviz import savegraph
+from dagflow.lib.Array import Array
+from dagflow.lib.RenormalizeDiag import RenormalizeDiag
+
+
+@mark.parametrize("dtype", ("d", "f"))
+@mark.parametrize("mode", ("diag", "offdiag"))
+@mark.parametrize("ndiag", (1, 2, 3, 4))
+def test_RenormalizeDiag(testname, ndiag, dtype, mode):
+    size = 4
+    scale = 2
+
+    matrix = ones(shape=(size, size), dtype=dtype)
+    with Graph(close=True) as graph:
+        array_matrix = Array("matrix", matrix)
+        scale_diag = Array("scale", [scale])
+
+        prod = RenormalizeDiag("RenormalizeDiag", mode=mode, ndiag=ndiag)
+        array_matrix >> prod
+        scale_diag >> prod("scale")
+
+    actual = prod.get_data()
+    # NOTE: uncomment to print input and result
+    print(f"matrix:\n{matrix}")
+    print(f"result:\n{actual}")
+    assert allclose(
+        tuple(actual[i].sum() for i in range(actual.shape[0])),
+        [1] * size,
+        atol=finfo(dtype).resolution,
+        rtol=0,
+    )
+
+    ograph = f"output/{testname}.png"
+    print(f"Write graph: {ograph}")
+    savegraph(graph, ograph, show="all")
