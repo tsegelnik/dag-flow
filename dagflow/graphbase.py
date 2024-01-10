eab55089482e26fb0fa91fb3a30d3f68142c8224
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-01-10 15:09:07 +0300

    chore: GraphBase, prevent same node being added multiple times
    (it is unclear how this happens though)

--- dagflow/graphbase.py
+++ dagflow/graphbase.py
@@ -2,14 +2,25 @@ from .shift import rshift
 
 
 class GraphBase:
-    __slots__ = ("_nodes",)
+    __slots__ = (
+        "_nodes",
+        "_nodes_set",
+    )
     _nodes: list
+    _nodes_set: set
 
     def __init__(self, *args):
         self._nodes = list(args)
+        self._nodes_set = set(args)
+
+        if len(self._nodes_set)!=len(self._nodes):
+            raise RuntimeError("There are duplicated nodes")
 
     def register_node(self, node):
+        if node in self._nodes_set:
+            return
         self._nodes.append(node)
+        self._nodes_set.add(node)
 
     def _wrap_fcns(self, *args):
         for node in self._nodes:
