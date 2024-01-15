e1f335b94cd6684358042da8e579a9a789fd625c
Tsegelnik Nikita <tsegelnik@jinr.ru>
2024-01-15 15:41:21 +0300

    improve test of RenormalizeDiag

--- test/nodes/test_RenormalizeDiag.py
+++ test/nodes/test_RenormalizeDiag.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from numpy import allclose, arange, finfo
+from numpy import allclose, arange, diag, finfo, zeros_like
 from pytest import mark
 
 from dagflow.graph import Graph
@@ -13,10 +13,10 @@ from dagflow.lib.RenormalizeDiag import RenormalizeDiag
 @mark.parametrize("mode", ("diag", "offdiag"))
 @mark.parametrize("ndiag", (1, 2, 3, 4))
 @mark.parametrize("scale", (2.0, 0.33))
-def test_RenormalizeDiag(testname, ndiag, dtype, mode, scale):
+def test_RenormalizeDiag(testname, debug_graph, ndiag, dtype, mode, scale, debug=True):
     size = 4
     matrix = arange(size**2, dtype=dtype).reshape(size, size)
-    with Graph(close=True) as graph:
+    with Graph(close=True, debug=debug_graph) as graph:
         array_matrix = Array("matrix", matrix)
         scale_diag = Array("scale", [scale])
 
@@ -26,8 +26,9 @@ def test_RenormalizeDiag(testname, ndiag, dtype, mode, scale):
 
     actual = prod.get_data()
     # NOTE: uncomment to print input and result
-    # print(f"matrix:\n{matrix}")
-    # print(f"result:\n{actual}")
+    if debug:
+        print(f"matrix:\n{matrix}")
+        print(f"result:\n{actual}")
 
     atol = finfo(dtype).resolution
     # check that sum of every column equals 1
@@ -36,34 +37,27 @@ def test_RenormalizeDiag(testname, ndiag, dtype, mode, scale):
     )
 
     # check that we scaled correct diagonals
-    desired = matrix.copy()
-    idiag = 1
+    desired = zeros_like(matrix)
     if mode == "diag":
-        for i in range(size):
-            desired[i, i] *= scale
-        while idiag < ndiag:
-            i = 0
-            while i + idiag < size:
-                desired[i + idiag, i] *= scale
-                desired[i, i + idiag] *= scale
-                i += 1
-            idiag += 1
+        for idiag in range(-size + 1, size):
+            desired += (
+                diag(matrix.diagonal(idiag), idiag) * scale
+                if abs(idiag) < ndiag
+                else diag(matrix.diagonal(idiag), idiag)
+            )
     else:
-        desired *= scale
-        for i in range(size):
-            desired[i, i] = matrix[i, i]
-        while idiag < ndiag:
-            i = 0
-            while i + idiag < size:
-                desired[i + idiag, i] = matrix[i + idiag, i]
-                desired[i, i + idiag] = matrix[i, i + idiag]
-                i += 1
-            idiag += 1
+        for idiag in range(-size + 1, size):
+            desired += (
+                diag(matrix.diagonal(idiag), idiag) * scale
+                if abs(idiag) >= ndiag
+                else diag(matrix.diagonal(idiag), idiag)
+            )
     for icol in range(size):
         desired[:, icol] /= ssum if (ssum := desired[:, icol].sum()) != 0.0 else 1
 
     # NOTE: uncomment to print desired
-    # print(f"desired:\n{desired}")
+    if debug:
+        print(f"desired:\n{desired}")
     assert allclose(desired, actual, atol=atol, rtol=0)
 
     ograph = f"output/{testname}.png"
