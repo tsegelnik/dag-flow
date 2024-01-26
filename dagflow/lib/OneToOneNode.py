f1251ef60b9f457b65617c5470bf3c8ac0eac72b
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-01-26 16:22:40 +0300

    feat: OneToOne node, output based replicate

--- dagflow/lib/OneToOneNode.py
+++ dagflow/lib/OneToOneNode.py
@@ -1,11 +1,12 @@
-from ..inputhandler import MissingInputAddPair
-from ..nodes import FunctionNode
-from ..node import Node
-from ..storage import NodeStorage
+from collections.abc import Sequence
+from typing import Any, Optional
 
 from multikeydict.typing import KeyLike
 
-from typing import Tuple, Optional
+from ..inputhandler import MissingInputAddPair
+from ..node import Node
+from ..nodes import FunctionNode
+from ..storage import NodeStorage
 
 
 class OneToOneNode(FunctionNode):
@@ -33,8 +34,30 @@ class OneToOneNode(FunctionNode):
 
     @classmethod
     def replicate(
-        cls, name: str, replicate: Tuple[KeyLike, ...] = ((),), **kwargs
-    ) -> Tuple[Optional[Node], NodeStorage]:
+        cls,
+        name: str,
+        *args: NodeStorage | Any,
+        replicate: Sequence[KeyLike] | None = None,
+        **kwargs,
+    ) -> tuple[Optional[Node], NodeStorage]:
+        if args and replicate is not None:
+            raise RuntimeError("OneToOneNode.replicate can use either `args` or `replicate`")
+
+        if args:
+            return cls.replicate_from_args(name, *args, **kwargs)
+
+        if replicate is None:
+            replicate = ((),)
+
+        return cls.replicate_from_indices(name, replicate=replicate, **kwargs)
+
+    @classmethod
+    def replicate_from_indices(
+        cls,
+        name: str,
+        replicate: Sequence[KeyLike] = ((),),
+        **kwargs,
+    ) -> tuple[Optional[Node], NodeStorage]:
         storage = NodeStorage(default_containers=True)
         nodes = storage("nodes")
         inputs = storage("inputs")
@@ -54,6 +77,36 @@ class OneToOneNode(FunctionNode):
         NodeStorage.update_current(storage, strict=True)
 
         if len(replicate) == 1:
+            return instance, storage
+
+        return None, storage
+
+    @classmethod
+    def replicate_from_args(
+        cls,
+        name: str,
+        *args: NodeStorage | Any,
+        **kwargs,
+    ) -> tuple[Optional[Node], NodeStorage]:
+        storage = NodeStorage(default_containers=True)
+        nodes = storage("nodes")
+        outputs = storage("outputs")
+
+        from multikeydict.nestedmkdict import walkitems
+
+        nobjects = 0
+        for arg in args:
+            for key, obj in walkitems(arg):
+                outname = (name,) + key
+                instance = cls(".".join(outname), **kwargs)
+                nodes[outname] = instance
+                obj >> instance  # pyright: ignore [reportUnusedExpression]
+                outputs[outname] = instance.outputs[0]
+                nobjects += 1
+
+        NodeStorage.update_current(storage, strict=True)
+
+        if nobjects == 1:
             return instance, storage
 
         return None, storage
