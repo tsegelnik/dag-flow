9baf6dcfa21635519fb541f9d2c21c22b1cbaf25
ggeorge <577-ggeorge@users.noreply.git.jinr.ru>
2024-01-29 22:49:47 +0300

    fix(profiling): fix SleepingNode after code review
    		- rename from SleepyNode to SleepingNode
    		- add `__slots__`
    		- missing type hints
    		- add `if TYPE_CHEKING...`
    		- rearrange code from _typefunc and __init__

--- dagflow/tools/profiling/__init__.py
+++ dagflow/tools/profiling/__init__.py
@@ -1,9 +1,9 @@
 from .individual import IndividualProfiling
 from .framework import FrameworkProfiling
-from .SleepyNode import SleepyNode 
+from .SleepyNode import SleepingNode
 
 __all__ = [
     "IndividualProfiling",
     "FrameworkProfiling",
     "SleepyNode"
-]   
+]
