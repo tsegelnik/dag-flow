fde66c7a52fc396a71c9e30c3d166c3264a702ba
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-02-02 17:15:59 +0300

    feat: new load_array (file_reader)

--- dagflow/bundles/load_array.py
+++ dagflow/bundles/load_array.py
@@ -1,8 +1,7 @@
 from collections.abc import Callable, Mapping
 from pathlib import Path
-from typing import Optional
+from typing import Mapping, Optional
 
-from numpy import allclose
 from schema import And
 from schema import Optional as SchemaOptional
 from schema import Or, Schema, Use
@@ -25,10 +24,6 @@ _schema_cfg = Schema(
     {
         "name": str,
         "filenames": And(IsFilenameSeqOrFilename, AllFileswithExt(*file_readers.keys())),
-        SchemaOptional("merge_x", default=False): bool,
-        SchemaOptional("x", default="x"): str,
-        SchemaOptional("y", default="y"): str,
-        SchemaOptional("normalize", default=False): bool,
         SchemaOptional("replicate", default=((),)): Or((IsStrSeqOrStr,), [IsStrSeqOrStr]),
         SchemaOptional("replicate_files", default=((),)): Or((IsStrSeqOrStr,), [IsStrSeqOrStr]),
         SchemaOptional("skip", default=None): And(
@@ -57,22 +52,17 @@ def _validate_cfg(cfg):
         return _schema_cfg.validate(cfg)
 
 
-def load_hist(acfg: Optional[Mapping] = None, **kwargs):
+def load_array(acfg: Optional[Mapping] = None, **kwargs):
     acfg = dict(acfg or {}, **kwargs)
     cfg = _validate_cfg(acfg)
 
-    name = cfg["name"]
+    name = (cfg["name"],)
     filenames = cfg["filenames"]
     keys = cfg["replicate"]
     file_keys = cfg["replicate_files"]
     objectname = cfg["objects"]
     skip = cfg["skip"]
-    normalize = cfg["normalize"]
 
-    xname = name, cfg["x"]
-    yname = name, cfg["y"]
-
-    edges_list = []
     data = {}
     for filekey, filename, objectkey, key in iterate_filenames_and_objectnames(
         filenames, file_keys, keys, skip=skip
@@ -80,34 +70,11 @@ def load_hist(acfg: Optional[Mapping] = None, **kwargs):
         skey = strkey(key)
         logger.log(INFO3, f"Process {skey}")
 
-        x, y = FileReader.hist[filename, objectname(skey, key)]
-        if normalize and (ysum := y.sum()) != 0.0:
-            y /= ysum
-            logger.log(INFO3, f"[normalize]")
-
-        data[key] = x, y
-        edges_list.append(x)
-
-    if cfg["merge_x"]:
-        x0 = edges_list[0]
-        for xi in edges_list[1:]:
-            if not allclose(x0, xi, atol=0, rtol=0):
-                raise RuntimeError("load_hist: inconsistent x axes, unable to merge.")
-
-        commonedges, _ = Array.make_stored(".".join(xname), x0)
-    else:
-        commonedges = None
-
+        data[key] = FileReader.array[filename, objectname(skey, key)]
     storage = NodeStorage(default_containers=True)
     with storage:
-        for key, (x, y) in data.items():
-            if commonedges:
-                edges = commonedges
-            else:
-                xkey = ".".join(xname + key)
-                edges, _ = Array.make_stored(xkey, x)
-            ykey = ".".join(yname + key)
-            Array.make_stored(ykey, y, edges=edges)
+        for key, array in data.items():
+            Array.make_stored(name+key, array)
 
     NodeStorage.update_current(storage, strict=True)
 
