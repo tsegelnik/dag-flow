dbc62d8809b033fdfe2aca0dd5bec7531a5fa55d
Tsegelnik Nikita <tsegelnik@jinr.ru>
2024-02-06 18:37:14 +0300

    fix Jacobian

--- dagflow/lib/Jacobian.py
+++ dagflow/lib/Jacobian.py
@@ -1,84 +1,81 @@
-from typing import TYPE_CHECKING
 from collections.abc import Sequence
+from typing import TYPE_CHECKING
 
 from numpy.typing import NDArray
 
 from ..exception import InitializationError
-from ..nodes import FunctionNode
-from ..parameters import Parameter, Parameters
+from ..parameters import GaussianParameter
+from .OneToOneNode import OneToOneNode
 
 if TYPE_CHECKING:
     from ..input import Input
-    from ..output import Output
 
 
-class Jacobian(FunctionNode):
-    __slots__ = ("_func", "_jacobian", "_reldelta", "_step", "_parameters_list")
+class Jacobian(OneToOneNode):
+    __slots__ = ("_scale", "_parameters_list")
 
-    _func: "Input"
-    _jacobian: "Output"
-    _reldelta: float
-    _step: float
-    _parameters_list: list[Parameter]
+    _scale: float
+    _parameters_list: list[GaussianParameter]
 
     def __init__(
         self,
         name,
-        reldelta: float = 0.1,
-        step: float = 0.1,
-        parameters: Sequence[Parameter] | None = None,
+        scale: float = 0.1,
+        parameters: Sequence[GaussianParameter] | None = None,
         **kwargs,
     ) -> None:
-        super().__init__(name, **kwargs, allowed_kw_inputs=("func",))
-        # TODO: do we need a check of step and reldelta values?
-        self._reldelta = reldelta
-        self._step = step
-
-        self._func = self._add_input("func")
-        self._jacobian = self._add_output("jacobian")
+        super().__init__(name, **kwargs)
+        self._scale = scale
 
         self._parameters_list = []  # pyright: ignore
         if parameters:
-            if isinstance(parameters, Parameters):
-                for par in parameters._pars:
-                    self.append_par(par)
-            elif isinstance(parameters, Sequence):
-                for par in parameters:
-                    self.append_par(par)
-            else:
+            if not isinstance(parameters, Sequence):
                 raise InitializationError(
-                    f"parameters must be a sequence of Parameters, but given {parameters=},"
+                    f"parameters must be a sequence of GaussianParameter, but given {parameters=},"
                     f" {type(parameters)=}!"
                 )
+            for par in parameters:
+                self.append_par(par)
 
-    def append_par(self, par: Parameter) -> None:
-        if not isinstance(par, Parameter):
-            raise RuntimeError(f"par must be a Parameter, but given {par=}, {type(par)=}!")
+    def append_par(self, par: GaussianParameter) -> None:
+        if not isinstance(par, GaussianParameter):
+            raise RuntimeError(f"par must be a GaussianParameter, but given {par=}, {type(par)=}!")
         self._parameters_list.append(par)
 
     def _typefunc(self) -> None:
-        self._jacobian.dd.dtype = "d"
-        self._jacobian.dd.shape = (self._func.dd.size, len(self._parameters_list))
-        # TODO: do we need to check smth?
+        n = len(self._parameters_list)
+        for inp, out in zip(self.inputs, self.outputs):
+            out.dd.dtype = inp.dd.dtype
+            out.dd.shape = (inp.dd.size, n)
 
     def _fcn(self):
-        reldelta_corrected = self._reldelta * self._step
-        f1 = 4.0 / (3.0 * reldelta_corrected)
-        f2 = 1.0 / (6.0 * reldelta_corrected)
-        res = self._jacobian.data
-        for i, parameter in enumerate(self._parameters_list):
-            # TODO: check coefficients
-            x0 = parameter.value
-            self._do_step(i, parameter, res, self._func, reldelta_corrected / 2.0, f1)
-            self._do_step(i, parameter, res, self._func, -reldelta_corrected / 2.0, -f1)
-            self._do_step(i, parameter, res, self._func, reldelta_corrected, -f2)
-            self._do_step(i, parameter, res, self._func, -reldelta_corrected, f2)
-            parameter.value = x0
+        c1 = 4.0 / 3.0
+        c2 = 1.0 / 6.0
+        for inp, outdata in zip(self.inputs, self.outputs.iter_data()):
+            for i, parameter in enumerate(self._parameters_list):
+                reldelta = parameter.sigma * self._scale
+                f1 = c1 / reldelta
+                f2 = c2 / reldelta
+
+                x0 = parameter.value
+                self._do_step(i, parameter, x0 + 0.5 * reldelta, f1, inp, outdata)
+                self._do_step(i, parameter, x0 - 0.5 * reldelta, -f1, inp, outdata)
+                self._do_step(i, parameter, x0 + reldelta, -f2, inp, outdata)
+                self._do_step(i, parameter, x0 - reldelta, f2, inp, outdata)
+                parameter.value = x0
+                inp.touch()
 
     def _do_step(
-        self, i: int, param: Parameter, res: NDArray, func: "Input", diff: float, coeff: float
+        self,
+        i: int,
+        param: GaussianParameter,
+        newval: float,
+        coeff: float,
+        inp: "Input",
+        res: NDArray,
     ):
-        param.value += diff
-        # TODO: do we need to touch, actually?
-        # func.touch()
-        res[:, i] += coeff * func.data[:]
+        param.value = newval
+        inp.touch()
+        inpdata = inp.data
+        for j in range(inp.dd.size):
+            res[j, i] += coeff * inpdata[j]
--- test/nodes/test_Jacobian.py
+++ test/nodes/test_Jacobian.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python
 
-from numpy import allclose, arange, eye, finfo
+from numpy import allclose, arange, eye, finfo, ones
 from pytest import mark
 
 from dagflow.graph import Graph
 from dagflow.graphviz import savegraph
+from dagflow.lib.Array import Array
+from dagflow.lib.Concatenation import Concatenation
 from dagflow.lib.Jacobian import Jacobian
-from dagflow.parameters import Parameters
+from dagflow.parameters import GaussianParameter
 
 
 @mark.parametrize("dtype", ("d", "f"))
-@mark.parametrize("parameters_mode", ("list", "Parameters"))
-def test_Jacobian(dtype, parameters_mode, testname):
+def test_Jacobian_01(dtype, testname):
     size = 10
-    reldelta = 0.1
-    step = 0.1
+    scale = 0.5
     values = arange(size, dtype=dtype)
-    names = tuple(f"par_{i:02d}" for i in range(size))
+    sigmas = ones(size, dtype=dtype)
 
     with Graph(close=True) as graph:
-        pars = Parameters.from_numbers(value=values, names=names, dtype=dtype)
+        valueslist = [Array(f"arr_{i:02d}", [val]) for i, val in enumerate(values)]
+        sigmaslist = [Array(f"sigma_{i:02d}", [sigma]) for i, sigma in enumerate(sigmas)]
+        pars = [
+            GaussianParameter(
+                parent=None,
+                value_output=value._output,
+                central_output=value._output,
+                normvalue_output=value._output,
+                sigma_output=sigma._output,
+            )
+            for value, sigma in zip(valueslist, sigmaslist)
+        ]
         jac = Jacobian(
             "Jacobian",
-            reldelta=reldelta,
-            step=step,
-            parameters=pars if parameters_mode == "Parameters" else pars._pars,
+            scale=scale,
+            parameters=pars,
         )
-        pars._value_node.outputs[0] >> jac("func")
+        parsconcat = Concatenation("parameters")
+        valueslist >> parsconcat
+        parsconcat >> jac
 
-    res = jac._jacobian.data
-    assert allclose(res, eye(size), atol=finfo(dtype).resolution, rtol=0)
+    res = jac.outputs[0].data
+    print(res)
+    assert allclose(res, eye(size), atol=10 * finfo(dtype).resolution, rtol=0)
 
     savegraph(graph, f"output/{testname}.png")
