d5950b7db0b257f4b554bfb86ebeaf6b8566740b
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-02-07 12:11:29 +0300

    chore: merge load_graph and load_graph_data

--- dagflow/bundles/load_graph.py
+++ dagflow/bundles/load_graph.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from collections.abc import Callable, Mapping
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from numpy import allclose
 from schema import And
@@ -20,6 +23,11 @@ from ..tools.schema import (
 )
 from .file_reader import FileReader, file_readers, iterate_filenames_and_objectnames
 
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    from multikeydict.typing import TupleKey
+
 _schema_cfg = Schema(
     {
         "name": str,
@@ -32,6 +40,7 @@ _schema_cfg = Schema(
         SchemaOptional("skip", default=None): And(
             Or(((str,),), [[str]]), Use(lambda l: tuple(set(k) for k in l))
         ),
+        SchemaOptional("index_order", default=None): Or((int,), [int]),
         SchemaOptional("objects", default=lambda: lambda st, tpl: st): Or(
             Callable, And({str: str}, Use(lambda dct: lambda st, tpl: dct.get(st, st)))
         ),
@@ -55,7 +64,9 @@ def _validate_cfg(cfg):
         return _schema_cfg.validate(cfg)
 
 
-def load_graph(acfg: Mapping | None = None, **kwargs):
+def _load_graph_data(
+    acfg: Mapping | None = None, **kwargs
+) -> tuple[tuple, tuple, NDArray | None, dict[TupleKey, tuple[NDArray, NDArray]]]:
     acfg = dict(acfg or {}, **kwargs)
     cfg = _validate_cfg(acfg)
 
@@ -65,12 +76,13 @@ def load_graph(acfg: Mapping | None = None, **kwargs):
     file_keys = cfg["replicate_files"]
     objectname = cfg["objects"]
     skip = cfg["skip"]
+    index_order = cfg["index_order"]
 
     xname = name, cfg["x"]
     yname = name, cfg["y"]
 
-    meshes_list = []
-    data = {}
+    meshes_list: list[NDArray] = []
+    data: dict[TupleKey, tuple[NDArray, NDArray]] = {}
     for _, filename, _, key in iterate_filenames_and_objectnames(
         filenames, file_keys, keys, skip=skip
     ):
@@ -88,20 +100,46 @@ def load_graph(acfg: Mapping | None = None, **kwargs):
             if not allclose(x0, xi, atol=0, rtol=0):
                 raise RuntimeError("load_graph: inconsistent x axes, unable to merge.")
 
-        commonmesh, _ = Array.make_stored(".".join(xname), x0)
-    else:
-        commonmesh = None
+        return xname, yname, x0, data
+
+    return xname, yname, None, data
+
+
+def load_graph(acfg: Mapping | None = None, **kwargs):
+    xname, yname, mesh_common, data = _load_graph_data(acfg, **kwargs)
 
     storage = NodeStorage(default_containers=True)
     with storage:
+        if mesh_common is not None:
+            mesh, _ = Array.make_stored(strkey(xname), mesh_common)
+        else:
+            mesh = None
+
         for key, (x, y) in data.items():
-            if commonmesh:
-                mesh = commonmesh
-            else:
-                xkey = ".".join(xname + key)
+            if mesh_common is None:
+                xkey = strkey(xname + key)
                 mesh, _ = Array.make_stored(xkey, x)
-            ykey = ".".join(yname + key)
+            ykey = strkey(yname + key)
             Array.make_stored(ykey, y, meshes=mesh)
+
+    NodeStorage.update_current(storage, strict=True)
+
+    return storage
+
+
+def load_graph_data(acfg: Mapping | None = None, **kwargs):
+    xname, yname, mesh_common, data = _load_graph_data(acfg, **kwargs)
+
+    storage = NodeStorage(default_containers=True)
+    data_storage = storage("data")
+    if mesh_common is None:
+        for key, (x, y) in data.items():
+            data_storage[xname + key] = x
+    else:
+        data_storage[xname] = mesh_common
+
+    for key, (x, y) in data.items():
+        data_storage[yname + key] = y
 
     NodeStorage.update_current(storage, strict=True)
 
