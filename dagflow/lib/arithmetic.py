b35dcb676d969f33f4b8c4eac4f16bf78d26abab
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-09-13 17:27:02 +0300

    feat: optimize Sum to cache data and nodes

--- dagflow/lib/arithmetic.py
+++ dagflow/lib/arithmetic.py
@@ -1,4 +1,7 @@
+from typing import Callable
+
 from numpy import add, copyto, divide, multiply, sqrt, square
+from numpy.typing import NDArray
 
 from .ManyToOneNode import ManyToOneNode
 from .OneToOneNode import OneToOneNode
@@ -7,7 +10,17 @@ from .OneToOneNode import OneToOneNode
 class Sum(ManyToOneNode):
     """Sum of all the inputs together"""
 
-    __slots__ = ()
+    __slots__ = (
+        "_input_node_callbacks",
+        "_input_data0",
+        "_input_data",
+        "_output_data",
+    )
+
+    _input_node_callbacks: list[Callable]
+    _input_data0: NDArray
+    _input_data: list[NDArray]
+    _output_data: NDArray
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("broadcastable", True)
@@ -15,10 +28,25 @@ class Sum(ManyToOneNode):
         self._labels.setdefault("mark", "Σ")
 
     def _fcn(self):
-        out = self.outputs["result"].data
-        copyto(out, self.inputs[0].data)
-        for _input in self.inputs[1:]:
-            add(out, _input.data, out=out)
+        for callback in self._input_node_callbacks:
+            callback()
+
+        copyto(self._output_data, self._input_data0)
+        for input_data in self._input_data:
+            add(self._output_data, input_data, out=self._output_data)
+
+    def _post_allocate(self):
+        self._input_node_callbacks = []
+        self._input_data = []
+        for input in self.inputs:
+            node = input.parent_node
+            if not node in self._input_node_callbacks:
+                self._input_node_callbacks.append(node.touch)
+
+            self._input_data.append(input.data_unsafe)
+
+        self._input_data0, self._input_data = self._input_data[0], self._input_data[1:]
+        self._output_data = self.outputs["result"].data_unsafe
 
 
 class Product(ManyToOneNode):
