ab354c9ae65330e788e753020def54bc40d7f6bc
Vitalii <zavadskiy.vs@phystech.edu>
2024-10-09 18:36:04 +0300

    feature: update test

--- tests/nodes/test_Proxy.py
+++ tests/nodes/test_Proxy.py
@@ -6,22 +6,37 @@ from dagflow.graph import Graph
 from dagflow.graphviz import savegraph
 from dagflow.lib import Array, Proxy
 
+from dagflow.logger import DEBUG
+from dagflow.logger import set_level
+
+set_level(DEBUG)
+
 
 def test_Proxy(testname, debug_graph):
     np_array0 = array([1, 2, 3, 4, 5])
     np_array1 = array([0, 1, 2, 3, 4])
 
-    with Graph(close_on_exit=True, debug=debug_graph) as graph:
+    with Graph(close_on_exit=True) as graph:
         array0 = Array("Array 0", np_array0)
-        array1 = Array("Array 1", np_array1)
         proxy = Proxy("proxy node")
         array0 >> proxy
-        array1.touch()
-
-    array1 >> proxy
+        # array1 = Array("Array 1", np_array1)
+        # array1 >> proxy
 
     assert allclose(proxy.get_data(), np_array0)
     savegraph(graph, f"output/{testname}-0.png", show="all")
+
+    with graph:
+        array1 = Array("Array 1", np_array1)
+        array1 >> proxy
+        proxy.open()
+        proxy.close()
+
     proxy.switch_input(1)
     assert allclose(proxy.get_data(), np_array1)
     savegraph(graph, f"output/{testname}-1.png", show="all")
+
+    proxy.switch_input(0)
+    assert allclose(proxy.get_data(), np_array0)
+    savegraph(graph, f"output/{testname}-2.png", show="all")
+
