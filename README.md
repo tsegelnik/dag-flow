289fb9b582ae3e7db0de67429b83ffc171115e68
Tsegelnik Nikita <tsgelnik@jinr.ru>
2025-05-24 16:57:42 +0300

    remove extra build files, update example and README.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Summary
+# The DAGFlow package
+## Summary
 
 [![python](https://img.shields.io/badge/python-3.10-purple.svg)](https://www.python.org/)
 [![pipeline](https://git.jinr.ru/dag-computing/dag-flow/badges/master/pipeline.svg)](https://git.jinr.ru/dag-computing/dag-flow/commits/master)
@@ -7,16 +8,73 @@
 [![pages](https://img.shields.io/badge/pages-link-white.svg)](http://dag-computing.pages.jinr.ru/dag-flow)
 -->
 
-DAGFlow is python implementation of dataflow programming with lazy graph evaluation.
+The **DAGFlow** software is a python implementation of the dataflow programming with the lazy graph evaluation approach.
 
 Main goals:
-*  Lazy evaluated directed acyclic graph
-*  Concise connection syntax
-*  Plotting with graphviz
+*  Lazy evaluated directed acyclic graph;
+*  Concise connection syntax;
+*  Plotting with graphviz;
 *  Flexibility. The goal of DAGFlow is not to be efficient, but rather flexible.
 
-The framework is intented to be used for the statistical analysis of the data of JUNO and Daya Bay neutrino oscillation experiments.
-The modules, located in the [project](https://git.jinr.ru/dag-computing) include:
+The framework is intented to be used for the statistical analysis of the data of *JUNO* and *Daya Bay* neutrino oscillation experiments.
+
+## Installation
+
+### For users (*recommended*)
+
+For regular use, it's best to install [the latest version of the project that's available on PyPi](https://pypi.org/project/dagmodelling/):
+```bash
+pip install dagmodelling
+```
+
+### For developers
+
+We recommend that developers install the package locally in editable mode:
+```bash
+git clone https://github.com/dagflow-team/dag-flow.git
+cd dag-flow
+pip install -e .
+```
+This way, the system will track all the changes made to the source files. This means that developers won't need to reinstall the package or set environment variables, even when a branch is changed.
+
+## Example
+
+For example, let's consider a sum of three input nodes and then a product of the result with another array.
+
+```python
+from numpy import arange
+
+from dagflow.core.graph import Graph
+from dagflow.plot.graphviz import savegraph
+from dagflow.lib.common import Array
+from dagflow.lib.arithmetic import Sum, Product
+
+# Define a source data
+array = arange(3, dtype="d")
+
+# Check predefined Array, Sum and Product
+with Graph(debug=debug) as graph:
+    # Define nodes
+    (in1, in2, in3, in4) = (Array(name, array) for name in ("n1", "n2", "n3", "n4"))
+    s = Sum("sum")
+    m = Product("product")
+
+    # Connect nodes
+    (in1, in2, in3) >> s
+    (in4, s) >> m
+    graph.close()
+
+    print("Result:", m.outputs["result"].data) # must print [0. 3. 12.]
+    savegraph(graph, "dagflow_example_1a.png")
+```
+The printed result must be `[0. 3. 12.]`, and the created image looks as
+![](https://github.com/dagflow-team/dag-flow/tree/0.9.0/example/dagflow_example_1a.png)
+
+
+For more examples see [example/example.py](https://github.com/dagflow-team/dag-flow/blob/master/example/example.py) or [tests](https://github.com/dagflow-team/dag-flow/tree/master/tests).
+
+## Additional modules
+
 - Supplementary python modules:
     * [dagflow-reactornueosc](https://git.jinr.ru/dag-computing/dagflow-reactorenueosc) — nodes related to reactor neutrino oscillations
     * [dagflow-detector](https://git.jinr.ru/dag-computing/dagflow-detector) — nodes for the detector responce modelling
--- example/example.py
+++ example/example.py
@@ -1,47 +1,14 @@
-from numpy import arange
-from numpy import copyto
-from numpy import result_type
-
 from dagflow.core.graph import Graph
-from dagflow.plot.graphviz import savegraph
 from dagflow.core.input_strategy import AddNewInputAddNewOutputForBlock
-from dagflow.lib.common import Array
-from dagflow.lib.arithmetic import Product
-from dagflow.lib.arithmetic import Sum
-from dagflow.lib.summation import WeightedSum
 from dagflow.core.node import Node
+from dagflow.lib.arithmetic import Product, Sum
+from dagflow.lib.common import Array
+from dagflow.lib.summation import WeightedSum
+from dagflow.plot.graphviz import savegraph
+from numpy import arange, copyto, result_type
 
 array = arange(3, dtype="d")
 debug = False
-
-
-class ThreeInputsOneOutput(Node):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("input_strategy", AddNewInputAddNewOutputForBlock())
-        super().__init__(*args, **kwargs)
-
-    def _function(self, _, inputs, outputs):
-        for i, output in enumerate(outputs):
-            out = output.data
-            copyto(out, inputs[3 * i].data)
-            for input in inputs[3 * i + 1 : (i + 1) * 3]:
-                out += input.data
-        return out
-
-    @property
-    def result(self):
-        return [out.data for out in self.outputs]
-
-    def _type_function(self) -> None:
-        """A output takes this function to determine the dtype and shape"""
-        for i, output in enumerate(self.outputs):
-            inputs = self.inputs[2 * i : 2 * (1 + i)]
-            output.dd.shape = inputs[0].dd.shape
-            output.dd.dtype = result_type(tuple(inp.dd.dtype for inp in inputs))
-        self.logger.debug(
-            f"Node '{self.name}': dtype={tuple(out.dd.dtype for out in self.outputs)}, "
-            f"shape={tuple(out.dd.shape for out in self.outputs)}"
-        )
 
 
 # Check predefined Array, Sum and Product
@@ -97,9 +64,7 @@ with Graph(debug=debug) as graph:
 
     (in1, in2) >> s  # [0,2,4]
     (in3, in4) >> ws
-    {"weight": weight} >> ws  # [0,1,2] * 2 + [0,1,2] * 3 = [0,5,10]
-    # NOTE: also it is possible to use the old style binding:
-    # weight >> ws("weight")
+    weight >> ws("weight")
     (s, ws) >> m  # [0,2,4] * [0,5,10] = [0,10,40]
     graph.close()
 
@@ -107,6 +72,39 @@ with Graph(debug=debug) as graph:
     savegraph(graph, "dagflow_example_3.png")
 
 
+# Create a custom node
+class ThreeInputsOneOutput(Node):
+    """The node sums every three inputs into a new output"""
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("input_strategy", AddNewInputAddNewOutputForBlock())
+        super().__init__(*args, **kwargs)
+
+    def _function(self):
+        for i, output in enumerate(self.outputs):
+            out = output._data
+            copyto(out, self.inputs[3 * i].data)
+            for input in self.inputs[3 * i + 1 : (i + 1) * 3]:
+                out += input.data
+        return out
+
+    @property
+    def result(self):
+        return [out.data for out in self.outputs]
+
+    def _type_function(self) -> None:
+        """A output takes this function to determine the dtype and shape"""
+        for i, output in enumerate(self.outputs):
+            inputs = self.inputs[2 * i : 2 * (1 + i)]
+            output.dd.shape = inputs[0].dd.shape
+            output.dd.dtype = result_type(tuple(inp.dd.dtype for inp in inputs))
+        self.logger.debug(
+            f"Node '{self.name}': dtype={tuple(out.dd.dtype for out in self.outputs)}, "
+            f"shape={tuple(out.dd.shape for out in self.outputs)}"
+        )
+
+
+# Test a custom node
 with Graph(debug=debug) as graph:
     (in1, in2, in3) = (Array(name, array) for name in ("n1", "n2", "n3"))
     (in4, in5, in6) = (Array(name, (1, 0, 0)) for name in ("n4", "n5", "n6"))
@@ -115,7 +113,7 @@ with Graph(debug=debug) as graph:
     (in1, in2, in3) >> s
     (in4, in5, in6) >> s
     (in7, in8, in9) >> s
-    graph.close()
+    s.close()
 
     print("Result:", s.result)
     savegraph(graph, "dagflow_example_4.png")
